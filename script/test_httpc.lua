cf6d807e6fc2f0358b4edff1bb899a810a79dd86
CandyMi <869646063@qq.com>
2019-01-07 19:49:49 +0800

    补充test_httpc.lua测试实例

---
+++ script/test_httpc.lua
@@ -0,0 +1,19 @@
+local Co = require "internal.Co"
+local timer = require "internal.Timer"
+local httpc = require "httpc"
+
+local ti = timer.timeout(2, function ( ... )
+    Co.spwan(function ( ... )
+        -- POST HEADER 为数组, BODY为数组
+        local code, body = httpc.post("http://localhost:8080/api", {{"Auth", "admin"}}, {{'page', 1}, {'limit', 10}})
+        print(code, body)
+
+        -- POST HEADER 为数组, BODY为字符串
+        local code, body = httpc.post("http://localhost:8080/api", {{"Auth", "admin"}}, "page=1&limit=10")
+        print(code, body)
+
+        -- POST HEADER 为数组, BODY为字符串
+        local code, body = httpc.get("http://localhost:8080/api?page=1&limit=10", {{"Auth", "admin"}})
+        print(code, body)
+    end)
+end)
