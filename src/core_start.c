88c178a6efa7532dd2fb73ba9d13cf678f3e636d
CandyMi <869646063@qq.com>
2019-01-14 21:04:55 +0800

    修复-d参数不存在的问题

--- src/core_start.c
+++ src/core_start.c
@@ -5,7 +5,7 @@ main(int argc, char const *argv[])
 {
 #if !defined(__APPLE__)
     /* 后台运行 */
-	if (argc == 1 && strcmp("", argv[argc-1])) daemon(0, 0);
+	if (argc == 1 && strcmp("-d", argv[argc-1])) daemon(0, 0);
 #endif
 	/* 系统初始化 */
 	core_sys_init();
