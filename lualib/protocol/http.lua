0e89cd08ec97d2dd5cab5136940bcb2b94b3ce4a
CandyMi <869646063@qq.com>
2019-01-16 11:44:25 +0800

    删除print测试代码

--- lualib/protocol/http.lua
+++ lualib/protocol/http.lua
@@ -560,7 +560,6 @@ function HTTP_PROTOCOL.EVENT_DISPATCH(fd, ipaddr, http)
 			if typ == HTTP_PROTOCOL.API or typ == HTTP_PROTOCOL.USE then
 				if type(cls) == "table" then
 					local c = cls:new({args = ARGS, file = FILE, method = METHOD, path = PATH, header = HEADER})
-					print(c)
 					ok, data = pcall(c, c)
 				else
 					ok, data = pcall(cls, {args = ARGS, file = FILE, method = METHOD, path = PATH, header = HEADER})
