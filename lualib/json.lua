9482d4709b66583fe92e94b69bce3f4807740264
CandyMi <869646063@qq.com>
2019-01-25 13:17:53 +0800

    修复增加一些cjson的注释说明

--- lualib/json.lua
+++ lualib/json.lua
@@ -1,7 +1,7 @@
 local cjson = require "cjson"
 
--- 让json默认解析收都用原表保存数据, 这也可以保证一个表的序列化与反序列化不会出现2种结果
--- cjson默认没有开启. 这里手动开启一下, 有需要的情况下可以手动关闭
+-- this lib fork from resty cjson, only modified some compatible codes
+-- more details please check it.
 cjson.decode_array_with_array_mt(true)
 
 local CJSON = {
@@ -21,10 +21,6 @@ local CJSON = {
 -- 设置稀疏数组用null填充
 function CJSON.sparse_array_to_null(array)
     return setmetatable(array, cjson.array_mt)
-end
-
-function CJSON.decode_array_with_array_mt(enable)
-    return cjson.decode_array_with_array_mt(enable)
 end
 
 return CJSON
