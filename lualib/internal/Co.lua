98a7e96a93979f498a282ab538f69fbc2cbbf625
CandyMi <869646063@qq.com>
2019-02-25 02:07:10 +0800

    模块化一些库文件

--- lualib/internal/Co.lua
+++ lualib/internal/Co.lua
@@ -1,9 +1,6 @@
-local log  = require "log"
-local task = require "task"
+-- 用于构建协程池
 
-local task_new = task.new
-local task_start = task.start
-
+local coroutine = coroutine
 local co_new = coroutine.create
 local co_start = coroutine.resume
 local co_wait = coroutine.yield
@@ -13,29 +10,11 @@ local co_self = coroutine.running
 local insert = table.insert
 local remove = table.remove
 
-local cos = {}
-
-local main_co = co_self()
-local main_task = task_new()
-
-local TASK_POOL = {}
-
-local function task_pop()
-	if #TASK_POOL > 0 then
-		return remove(TASK_POOL)
-	end
-	return task_new()
-end
-
-local function task_push(task)
-	return insert(TASK_POOL, task)
-end
-
-local CO_POOL = {}
+local Co = {}
 
 local function co_pop(func)
-	if #CO_POOL > 0 then
-		return remove(CO_POOL)
+	if #Co > 0 then
+		return remove(Co)
 	end
 	local co = co_new(func)
 	co_start(co)
@@ -43,70 +22,22 @@ local function co_pop(func)
 end
 
 local function co_push(co)
-	return insert(CO_POOL, co)
+	Co[#Co + 1] = co
 end
 
-local function f()
-	while 1 do 
-		local ok, msg = pcall(co_wait())
-		if not ok then
-			log.error(msg)
-		end
-		local co, main = co_self()
-		if not main then
-			task_push(cos[co])
-			co_push(co)
-			cos[co] = nil
-		end
-	end
+local function new(func)
+	return co_pop(func)
 end
 
-local Co = {}
-
--- 创建协程
-function Co.new(f)
-	return co_new(f)
+local function close(co)
+	return co_push(co)
 end
 
--- 查找
-function Co.self()
-	return co_self()
-end
-
--- 让出
-function Co.wait()
-	return co_wait()
-end
-
--- 启动
-function Co.spwan(func, ...)
-	if func and type(func) == "function" then
-		local co = co_pop(f)
-		cos[co] = task_pop()
-		return task_start(cos[co], co, func, ...)
-	end
-	error("Co Just Can Spwan a Coroutine to run in sometimes.")
-end
-
--- 唤醒
-function Co.wakeup(co, ...)
-	assert(co, "Attemp to Pass a nil value (need a co).")
-	local status = co_status(co)
-	if co == co_self() then
-		return log.error("Can't wakeup current co.")
-	end
-	if main_co == co and status == "suspended" then
-		return task_start(main_task, main_co, ...)
-	end
-	local t = cos[co]
-	if not t then
-		return log.error("Can't find co in co list.")
-	end
-	return task_start(t, co, ...)
-end
-
-function Co.count()
-	return #CO_POOL, #TASK_POOL
-end
-
-return Co
+return {
+	new = new,
+	close = close,
+	self = co_self,
+	wait = co_wait,
+	wakeup = co_start,
+	status = co_status,
+}
--- lualib/internal/TASK.lua
+++ lualib/internal/TASK.lua
@@ -1,131 +1,30 @@
-local class = require "class"
+-- 用于构建Task池
 local task = require "task"
 
 local task_new = task.new
 local task_stop = task.stop
 local task_start = task.start
 
-local coroutine = coroutine
-local co_new = coroutine.create
-local co_start = coroutine.resume
-local co_wait = coroutine.yield
-local co_status = coroutine.status
-local co_self = coroutine.running
-
--- TASK 状态
-local T_STATUS = {
-	WAIT     = 0,
-	PENDDING = 1,
-	RUNNING  = 2
-}
-
 local insert = table.insert
 local remove = table.remove
 
-local cos = {}
 
+local TASK = {}
 
-local main_co = co_self()
-local main_task = task_new()
-
-local TASK_POOL = {}
-
-local function task_pop()
-	if #TASK_POOL > 0 then
-		return remove(TASK_POOL)
+local function new(func)
+	if #TASK > 0 then
+		return remove(TASK)
 	end
 	return task_new()
 end
 
-local function task_add(task)
-	TASK_POOL[#TASK_POOL + 1] = task
+local function close(task)
+	return insert(TASK, task)
 end
 
-local CO_POOL = {}
-
-local function co_pop(func)
-	if #CO_POOL > 0 then
-		return remove(CO_POOL)
-	end
-	local co = co_new(func)
-	co_start(co)
-	return co
-end
-
-local function co_add(co)
-	CO_POOL[#CO_POOL + 1] = co
-end
-
-local TASK = class("TASK")
-
-function TASK:ctor(opt)
-	self.co = opt.co or co_pop()
-	self.task = opt.task or task_pop()
-	self.status = T_STATUS.WAIT
-end
-
--- 启动一个任务
-function TASK:start(...)
-
-end
-
--- 停止一个任务
-function TASK:stop(...)
-
-end
-
-function TASK:close(...)
-	if self.status ~= T_STATUS.WAIT then
-		task_stop(self.task)
-	end
-end
-
--- local Co = {}
-
--- -- 创建协程
--- function Co.new(f)
--- 	return co_new(f)
--- end
-
--- -- 查找
--- function Co.self()
--- 	return co_self()
--- end
-
--- -- 让出
--- function Co.wait()
--- 	return co_wait()
--- end
-
--- -- 启动
--- function Co.spwan(func, ...)
--- 	if func and type(func) == "function" then
--- 		local co = co_pop(f)
--- 		cos[co] = task_pop()
--- 		return task_start(cos[co], co, func, ...)
--- 	end
--- 	error("Co Just Can Spwan a Coroutine to run in sometimes.")
--- end
-
--- -- 唤醒
--- function Co.wakeup(co, ...)
--- 	assert(co, "Attemp to Pass a nil value (need a co).")
--- 	local status = co_status(co)
--- 	if co == co_self() then
--- 		return log.error("Can't wakeup current co.")
--- 	end
--- 	if main_co == co and status == "suspended" then
--- 		return task_start(main_task, main_co, ...)
--- 	end
--- 	local t = cos[co]
--- 	if not t then
--- 		return log.error("Can't find co in co list.")
--- 	end
--- 	return task_start(t, co, ...)
--- end
-
--- function Co.count()
--- 	return #CO_POOL, #TASK_POOL
--- end
-
-return TASK
+return {
+	new = new -- 创建Task
+	close = close -- 回收Task
+	stop = task_stop -- 停止Task
+	start = task_start -- 启动Task
+}
