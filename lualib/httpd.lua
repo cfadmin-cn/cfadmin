e4d7238d9e0b56d88eb8f28f68cae8c8b13d7a01
CandyMi <869646063@qq.com>
2019-04-03 01:03:11 +0800

    更新跨域探测支持, 解决options的一些问题与增加判断content-type为0的判断

--- lualib/httpd.lua
+++ lualib/httpd.lua
@@ -134,7 +134,7 @@ function httpd:log(path)
     log.outfile = self.logpath
 end
 
-function httpd:tolog(code, path, ip, ip_list, speed)
+function httpd:tolog(code, path, ip, ip_list, method, speed)
     if self.logpath then
         if not self.logfile then
             local err
@@ -143,13 +143,13 @@ function httpd:tolog(code, path, ip, ip_list, speed)
                 return log.error(self.logpath..":"..err)
             end
         end
-        local ok, err = self.logfile:write(fmt("[%s] - %s - %s - %s - %d - time: %0.6f/Sec\r\n", os_date("%Y/%m/%d %H:%M:%S"), ip, ip_list, path, code, speed))
+        local ok, err = self.logfile:write(fmt("[%s] - %s - %s - %s - %s - %d - time: %0.6f/Sec\r\n", os_date("%Y/%m/%d %H:%M:%S"), ip, ip_list, path, method, code, speed))
         if not ok then
             return log.error(self.logpath..":"..err)
         end
         self.logfile:flush()
     end
-    print(fmt("[%s] - %s - %s - %s - %d - time: %0.6f/Sec\r\n", os_date("%Y/%m/%d %H:%M:%S"), ip, ip_list, path, code, speed))
+    print(fmt("[%s] - %s - %s - %s - %s - %d - time: %0.6f/Sec\r\n", os_date("%Y/%m/%d %H:%M:%S"), ip, ip_list, path, method, code, speed))
 end
 
 -- 监听请求
