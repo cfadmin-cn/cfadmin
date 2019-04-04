09bf80cb28a434f36f9e243f4f59716f6fd57770
CandyMi <869646063@qq.com>
2019-04-04 17:15:21 +0800

    调整示例文件

--- script/test_MQ.lua
+++ script/test_MQ.lua
@@ -1,4 +1,5 @@
 -- 基于redis的订阅与发布消息队列(同步处理)
+local Timer = require "internal.Timer"
 local MQ = require "MQ"
 require "utils"
 
@@ -12,7 +13,16 @@ print(rds:on('/test/*', function (msg)
 	var_dump(msg)
 end))
 
+Timer.at(1, function ( ... )
+	rds:emit("/test/admin", '{"code":100}')
+end)
+
+Timer.sleep(10)
+print("停止消息队列监听与投递.")
+rds:close()
+
 -- 基于mqtt的订阅与发布消息队列(同步处理)
+local Timer = require "internal.Timer"
 local MQ = require "MQ"
 require "utils"
 
@@ -22,6 +32,14 @@ local mqtt = MQ:new {
 	type = 'mqtt'
 }
 
+Timer.at(0.1, function ( ... )
+	mqtt:emit("/test/admin", '{"code":100}')
+end)
+
 print(mqtt:on('/test/*', function (msg)
 	var_dump(msg)
 end))
+
+Timer.sleep(10)
+print("停止消息队列监听与投递.")
+mqtt:close()
