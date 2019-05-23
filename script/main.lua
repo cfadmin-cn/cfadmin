9d7226cbdd8e33c717c9a1fe5c9c9bee63a7f105
CandyMi <869646063@qq.com>
2019-05-24 00:49:58 +0800

    更新main.lua

--- script/main.lua
+++ script/main.lua
@@ -1,76 +1,92 @@
 local httpd = require "httpd"
-local http = require "httpd.http"
-local httpc = require "httpc"
+local DB = require "DB"
 
+--[[
+请按照以下步奏初始化后台:
+	1. 创建一个数据库(名字任意);
+	2. 请手动打开lualib/db/database.sql文件, 复制里面的SQL语句在GUI工具中执行一次;
+	3. 执行完成之后, 将您填写的数据库替换database字段, 并且charset需要设置一致.
+]]
 
+local db = DB:new({
+	host = '10.0.0.16',
+	port = 3306,
+	username = 'root',
+	password = '123456789',
+	charset = 'utf8',
+	database = 'cfadmin',
+	max = 100,
+})
+
+db:connect()
+
+-- 导入httpd对象
 local app = httpd:new("App")
+-- httpd启用Cookie扩展
+app:enable_cookie()
+-- httpd设置Cookie加密的密匙
+app:cookie_secure("https://github.com/CandyMi/core_framework")
+-- app:cookie_secure("candymi")
+
+-- 导入cf内置的admin库
+local cfadmin = require "admin"
+
+-- 注册后台页面路由
+cfadmin.init_page(app, db)
 
 
--- 每次http请求在处理函数之前将调用此方法, 第三方中间件可以在此针对回调函数设计.
--- 需要注意的是: 如果路由不存在, 则不会经过此回调!
--- 以下为使用示例:
--- 如果未使用before回调进行中间件设计或进行header验证, 请不要注册before回调
--- 只要注册了before回调, 即使不做任何操作(返回非法值(nil))也会返回http 401 code.
-app:before(function (content)
-	if true then
-		return http.ok()
-	end
-	-- if true then
-	-- 	return http.redirect('https://github.com/CandyMi/core_framework')
-	-- end
-	-- if true then
-	-- 	return http.throw(431, '<h1> This is 413 Error, too long request header</h1>')
-	-- end
+-- 这个函数仅在第一次初始化数据的时候适用
+-- 初始化完成之后, 请不要再运行.
+cfadmin.init_db()
+
+-- 这里设置首页的显示的页面
+-- cfadmin.init_home(location or domain + path)
+-- cfadmin.init_home('https://www.baidu.com')
+
+local view = require "admin.view"
+-- 参数:
+-- 1. ctx是一个http req 对象, 目前内置包括: get_method, get_args, get_path, get_raw_path, get_headers, get_cookie
+-- 2. db初始化后的db对象, 方便用户直接使用.
+
+view.use('/admin/test1', function (ctx, db)
+	print(view.get_locale(), view.get_cdn())
+	return "hello world"
 end)
 
--- 单个连接最大保持时间
-app:timeout(5)
-
--- 最大URI长度
-app:max_path_size(1024)
-
--- 最大Header长度
-app:max_header_size(65535)
-
--- 最大Body长度
-app:max_body_size(1024 * 1024)
-
--- 可自定义Server Name
-app:server_name('Candy Server/1.0')
-
--- 注册接口
-app:api("/api", require "api")
-
-app:api("/app", function (opt)
-	local code, resp = httpc.get('http://t.weather.sojson.com/api/weather/city/101030100')
-	if code ~= 200 then
-		print(code, resp)
-		return '{"code":500,"message":"请求失败."}'
-	end
-	return resp
+view.api('/api/admin/test2', function (ctx, db)
+	return '{"code":0,"msg":"hello world"}'
 end)
 
--- 注册普通路由(html/text)
-app:use("/view", function (opt)
-    return "<html><h1 align=center>This is text/html content-type<hr></h1><body align=center>Server: cf/0.1</body></html>"
-end)
+-- 这里是设置语言的地方
+-- 语言表在admin/locales内, 可参照key -> value进行填写.
+-- 传入一个数组表: 索引1是key, 索引2为显示内容.
 
--- 批量路由注册
-app:group(app.API, '/admin', require "admin")
+-- cfadmin.add_locale_item('ZH-CN', {
+-- 	{'login.form.title', '这是登录页Title'},
+-- 	{'dashboard.header.logo', '仪表盘 Logo'}
+-- })
 
--- 批量路由注册
-app:group(app.USE, '/login', require "admin")
+-- cfadmin.add_locale_item('EN-US', {
+-- 	{'login.form.title', 'This is Login Page Title'},
+-- 	{'dashboard.header.logo', 'dashboard Logo'}
+-- })
 
--- 注册websocket路由
-app:ws("/ws", require "ws")
+-- 开启页面缓存能显著提升页面渲染性能. 生产环境下建议开启.
+-- 也因为cf缓存模板页面内容, 所以开发模式下不建议开启.
+-- cfadmin.cached()
 
--- 注册静态文件目录
-app:static('static', 10)
+-- 这个方法可以用来设置静态文件域名与前缀.
+-- 如果静态文件在其它域名或者无法访问, 可以使用这个参数修改.(域名后必须加上'/')
+-- cfadmin.static('/')
 
--- 需要记录日志, 并且指定日志存放路径
--- app:log("./http.log")
+-- 设置cfadmin的区域语言, 默认为: ZH-CN
+-- cfadmin.set_locale('EN-US')
 
--- http监听端口
+-- 设置客户端静态文件ttl值内无需再次请求, 减少服务端消耗
+-- app:static('static', 30)
+app:static('static')
+
+-- httpd监听端口
 app:listen("0.0.0.0", 8080)
 
 -- 运行
