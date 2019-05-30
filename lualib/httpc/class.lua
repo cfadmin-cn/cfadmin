234eb73f06bee73a41955644894995bca2df5ad2
CandyMi <869646063@qq.com>
2019-05-30 15:05:42 +0800

    增加httpc的类封装, 用于socket复用场景

---
+++ lualib/httpc/class.lua
@@ -0,0 +1,235 @@
+local class = require "class"
+local cf = require "cf"
+local cf_fork = cf.fork
+local cf_wait = cf.wait
+local cf_wakeup = cf.wakeup
+
+local protocol = require "httpc.protocol"
+local sock_new = protocol.sock_new
+local sock_recv = protocol.sock_recv
+local sock_send = protocol.sock_send
+local sock_connect = protocol.sock_connect
+local httpc_response = protocol.httpc_response
+local splite_protocol = protocol.splite_protocol
+local build_get_req = protocol.build_get_req
+local build_post_req = protocol.build_post_req
+local build_json_req = protocol.build_json_req
+local build_file_req = protocol.build_file_req
+
+local type = type
+local assert = assert
+local ipairs = ipairs
+local tostring = tostring
+
+local random = math.random
+local find = string.find
+local match = string.match
+local split = string.sub
+local splite = string.gmatch
+local spliter = string.gsub
+local lower = string.lower
+local insert = table.insert
+local concat = table.concat
+local toint = math.tointeger
+local fmt = string.format
+
+local SERVER = "cf/0.1"
+
+local CRLF = '\x0d\x0a'
+local CRLF2 = '\x0d\x0a\x0d\x0a'
+
+
+
+local httpc = class("httpc")
+
+function httpc:ctor (opt)
+  self.server = "cf/0.1"
+  self.timeout = opt.timeout or 15
+end
+
+-- get 请求
+function httpc:get (domain, headers, args, timeout)
+  local opt, err = splite_protocol(domain)
+  if not opt then
+    return nil, err
+  end
+
+  if self.domain and self.domain ~= opt.domain then
+    return nil, "1. 不同的域名不可使用httpc对象来请求"
+  end
+
+  if self.port and self.port ~= opt.port then
+    return nil, "2. 不同的域名不可使用httpc对象来请求"
+  end
+
+  opt.args = args
+  opt.headers = headers
+  opt.server = self.server
+
+  self.domain = opt.domain
+  self.port = opt.port
+
+  local REQ = build_get_req(opt)
+
+  self.sock = self.sock or sock_new():timeout(self.timeout)
+
+  local ok, err = sock_connect(self.sock, opt.protocol, opt.domain, opt.port)
+  if not ok then
+    return ok, err
+  end
+  local ok, err = sock_send(self.sock, opt.protocol, REQ)
+  if not ok then
+    return ok, err
+  end
+  local code, msg = httpc_response(self.sock, opt.protocol)
+  if not code then
+    self.sock:close()
+    self.sock = nil
+  end
+  return code, msg
+end
+
+-- post 请求
+function httpc:post (domain, headers, body, timeout)
+  local opt, err = splite_protocol(domain)
+  if not opt then
+    return nil, err
+  end
+
+  if self.domain and self.domain ~= opt.domain then
+    return nil, "1. 不同的域名不可使用httpc对象来请求"
+  end
+
+  if self.port and self.port ~= opt.port then
+    return nil, "2. 不同的域名不可使用httpc对象来请求"
+  end
+
+  opt.body = body
+  opt.headers = headers
+  opt.server = self.server
+
+  self.domain = opt.domain
+  self.port = opt.port
+
+  local REQ = build_post_req(opt)
+
+  self.sock = self.sock or sock_new():timeout(self.timeout)
+
+  local ok, err = sock_connect(self.sock, opt.protocol, opt.domain, opt.port)
+  if not ok then
+    return ok, err
+  end
+  local ok, err = sock_send(self.sock, opt.protocol, REQ)
+  if not ok then
+    return ok, err
+  end
+  local code, msg = httpc_response(self.sock, opt.protocol)
+  if not code then
+    self.sock:close()
+    self.sock = nil
+  end
+  return code, msg
+end
+
+-- json 请求
+function httpc:json (domain, headers, json, timeout)
+
+  assert(type(json) == "string", "Please passed A vaild json string.")
+
+  local opt, err = splite_protocol(domain)
+  if not opt then
+    return nil, err
+  end
+
+  if self.domain and self.domain ~= opt.domain then
+    return nil, "1. 不同的域名不可使用httpc对象来请求"
+  end
+
+  if self.port and self.port ~= opt.port then
+    return nil, "2. 不同的域名不可使用httpc对象来请求"
+  end
+
+  opt.json = json
+  opt.headers = headers
+  opt.server = self.server
+
+  self.domain = opt.domain
+  self.port = opt.port
+
+  local REQ = build_json_req(opt)
+
+  self.sock = self.sock or sock_new():timeout(self.timeout)
+
+  local ok, err = sock_connect(self.sock, opt.protocol, opt.domain, opt.port)
+  if not ok then
+    return ok, err
+  end
+  local ok, err = sock_send(self.sock, opt.protocol, REQ)
+  if not ok then
+    return ok, err
+  end
+  local code, msg = httpc_response(self.sock, opt.protocol)
+  if not code then
+    self.sock:close()
+    self.sock = nil
+  end
+  return code, msg
+end
+
+-- file 请求
+function httpc:file (domain, headers, files, timeout)
+  local opt, err = splite_protocol(domain)
+  if not opt then
+    return nil, err
+  end
+
+  if self.domain and self.domain ~= opt.domain then
+    return nil, "1. 不同的域名不可使用httpc对象来请求"
+  end
+
+  if self.port and self.port ~= opt.port then
+    return nil, "2. 不同的域名不可使用httpc对象来请求"
+  end
+
+  opt.files = files
+  opt.headers = headers
+  opt.server = self.server
+
+  self.domain = opt.domain
+  self.port = opt.port
+
+  local REQ = build_file_req(opt)
+
+  self.sock = self.sock or sock_new():timeout(self.timeout)
+
+  local ok, err = sock_connect(self.sock, opt.protocol, opt.domain, opt.port)
+  if not ok then
+    return ok, err
+  end
+  local ok, err = sock_send(self.sock, opt.protocol, REQ)
+  if not ok then
+    return ok, err
+  end
+  local code, msg = httpc_response(self.sock, opt.protocol)
+  if not code then
+    self.sock:close()
+    self.sock = nil
+  end
+  return code, msg
+end
+
+-- 多次请求
+function httpc:multi_request (opt)
+  if type ~= 'table' then
+    return nil, "错误的参数类型"
+  end
+end
+
+function httpc:close ()
+  if self.sock then
+    self.sock:close()
+    self.sock = nil
+  end
+end
+
+return httpc
--- script/test_httpc.lua
+++ script/test_httpc.lua
@@ -1,23 +1,32 @@
 local cf = require "cf"
 local httpc = require "httpc"
 local json = require "json"
+local system = require "system"
+local now = system.now
+
+cf.fork(function (args)
+  local code, body = httpc.get("http://localhost:8080/api")
+  print(code, body)
+end)
 
 local ti = cf.timeout(2, function ( ... )
   cf.fork(function ( ... )
+    local t1 = now()
+    print("开始时间:", t1)
     -- GET请求: 在参数固定的情况下可以直接写在url内
     local code, body = httpc.get("http://localhost:8080/api?page=1&limit=10", {{"Auth", "admin"}})
     print(code, body)
 
     -- GET请求: 在参数为动态的情况下可以提供请求数组由httpc库进行拼接
-    local code, body = httpc.get("http://[::1]:8080/api", {{"Auth", "admin"}}, {{'page', 1}, {'limit', 10}})
+    local code, body = httpc.get("http://localhost:8080/api", {{"Auth", "admin"}}, {{'page', 1}, {'limit', 10}})
     print(code, body)
 
     -- POST HEADER 为数组, BODY为数组
-    local code, body = httpc.post("http://[::ffff:127.0.0.1]:8080/api", {{"Auth", "admin"}}, {{'page', 1}, {'limit', 10}})
+    local code, body = httpc.post("http://localhost:8080/api", {{"Auth", "admin"}}, {{'page', 1}, {'limit', 10}})
     print(code, body)
 
     -- POST HEADER 为数组, BODY为字符串
-    local code, body = httpc.post("http://127.0.0.1:8080/api", {{"Auth", "admin"}}, "page=1&limit=10")
+    local code, body = httpc.post("http://localhost:8080/api", {{"Auth", "admin"}}, "page=1&limit=10")
     print(code, body)
 
     -- http json请求示例
@@ -30,5 +39,47 @@ local ti = cf.timeout(2, function ( ... )
         {name='2', filename='2.jpg', file='2', type='abc'},
         })
     print(code, body)
+    local t2 = now()
+    print("结束时间:", t1, "总耗时:", t2 - t1)
+  end)
+end)
+
+
+require "utils"
+local http = require "httpc.class"
+local hc = http:new {}
+local ti = cf.timeout(1, function ( ... )
+  cf.fork(function ( ... )
+    local t1 = now()
+    print("开始时间:", t1)
+    -- GET请求: 在参数固定的情况下可以直接写在url内
+    local code, body = hc:get("http://localhost:8080/api?page=1&limit=10", {{"Auth", "admin"}})
+    print(code, body)
+
+    -- GET请求: 在参数为动态的情况下可以提供请求数组由httpc库进行拼接
+    local code, body = hc:get("http://localhost:8080/api", {{"Auth", "admin"}}, {{'page', 1}, {'limit', 10}})
+    print(code, body)
+
+    -- POST HEADER 为数组, BODY为数组
+    local code, body = hc:post("http://localhost:8080/api", {{"Auth", "admin"}}, {{'page', 1}, {'limit', 10}})
+    print(code, body)
+
+    -- POST HEADER 为数组, BODY为字符串
+    local code, body = hc:post("http://localhost:8080/api", {{"Auth", "admin"}}, "page=1&limit=10")
+    print(code, body)
+
+    -- http json请求示例
+    local code, body = hc:json("http://localhost:8080/api", {{"Auth", "admin"}}, json.encode({page=1, limit=10}))
+    print(code, body)
+
+    -- http 上传文件示例
+    local code, body = hc:file('http://localhost:8080/api', nil, {
+        {name='1', filename='1.jpg', file='1', type='abc'},
+        {name='2', filename='2.jpg', file='2', type='abc'},
+        })
+    print(code, body)
+    local t2 = now()
+    print("结束时间:", t1, "总耗时:", t2 - t1)
+    hc:close()
   end)
 end)
