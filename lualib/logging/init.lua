d87d2acb5942d0cd935396f6a8267bc2a9403dfd
CandyMi <869646063@qq.com>
2019-06-04 00:24:55 +0800

    优化日志打印

--- lualib/logging/init.lua
+++ lualib/logging/init.lua
@@ -3,7 +3,6 @@ local class = require "class"
 local system = require "system"
 local now = system.now
 local type = type
-local print = print
 local assert = assert
 local pairs = pairs
 local tostring = tostring
@@ -11,8 +10,10 @@ local getmetatable = getmetatable
 
 local modf = math.modf
 local debug_getinfo = debug.getinfo
-local os_date = os.date
+local os_date = require("sys").date
 local io_open = io.open
+local io_write = io.write
+local io_type = io.type
 local format = string.format
 local concat = table.concat
 local modf = math.modf
@@ -21,8 +22,7 @@ local modf = math.modf
 -- 格式化时间: [年-月-日 时:分:秒,毫秒]
 local function fmt_Y_m_d_H_M_S()
   local ts, f = modf(now())
-  f = format("%03.0f", f * 1e3)
-  return concat({'[', os_date('%Y-%m-%d %H:%M:%S'), ',', f, ']'})
+  return concat({'[', os_date('%Y-%m-%d %H:%M:%S'), ',', format("%0.3f", f * 1e3), ']'})
 end
 
 -- 格式化时间: [年-月-日 时:分:秒]
@@ -97,51 +97,63 @@ local Log = class("Log")
 
 function Log:ctor (opt)
   if type(opt) == 'table' then
-    self.to_dump = opt.dump
+    self.dumped = opt.dump
     self.path = opt.path
-    self.now = Y_m_d()
+    self.today = Y_m_d()
   end
 end
 
 -- 常规日志
 function Log:INFO (...)
-  print(FMT("\27[32m"..debuginfo(), "[INFO]".."\27[0m", ...))
+  io_write(FMT("\27[32m"..debuginfo(), "[INFO]".."\27[0m", ...))
   self:dump(FMT(debuginfo(), "[INFO]", ...))
 end
 
 -- 错误日志
 function Log:ERROR (...)
-  print(FMT("\27[31m"..debuginfo(), "[ERROR]".."\27[0m", ...))
+  io_write(FMT("\27[31m"..debuginfo(), "[ERROR]".."\27[0m", ...))
   self:dump(FMT(debuginfo(), "[ERROR]", ...))
 end
 
 -- 调试日志
 function Log:DEBUG (...)
-  print(FMT("\27[36m"..debuginfo(), "[DEBUG]".."\27[0m", ...))
+  io_write(FMT("\27[36m"..debuginfo(), "[DEBUG]".."\27[0m", ...))
   self:dump(FMT(debuginfo(), "[DEBUG]", ...))
 end
 
 -- 警告日志
 function Log:WARN (...)
-  print(FMT("\27[33m"..debuginfo(), "[WARN]".."\27[0m", ...))
+  io_write(FMT("\27[33m"..debuginfo(), "[WARN]".."\27[0m", ...))
   self:dump(FMT(debuginfo(), "[WARN]", ...))
 end
 
 -- dump日志到磁盘
 function Log:dump(log)
-  if self.to_dump and self.path then
-    if not self.file then
-      self.file, err = io_open('logs/'..self.path..'_'..self.now..'.log', 'a')
-    else
-      local now = Y_m_d()
-      if now ~= self.now then
-        self.file:close()
-        self.now = now
-        self.file = io_open('logs/'..self.path..'_'..self.now..'.log', 'a')
-      end
-    end
-    self.file:write(log):flush()
+  if not self.dumped or type(self.path) ~= 'string' then
+    return
   end
+  local today = Y_m_d()
+  if today ~= self.today then
+    if self.file then
+      self.file:close()
+      self.file = nil
+    end
+    local file, err = io_open('logs/'..self.path..'_'..today..'.log', 'a')
+    if not file then
+      return io_type(io.output()) == 'file' and io_write('打开文件失败.'..(err or '')..'\n')
+    end
+    self.file, self.today = file, today
+    file:setvbuf("line")
+  end
+  if not self.file then
+    local file, err = io_open('logs/'..self.path..'_'..today..'.log', 'a')
+    if not file then
+      return io_type(io.output()) == 'file' and io_write('打开文件失败.'..(err or '')..'\n')
+    end
+    file:setvbuf("line")
+    self.file = file
+  end
+  return self.file:write(log)
 end
 
 return Log
