2e0220cad39a082a7985843d3a194cd2ac263db8
CandyMi <869646063@qq.com>
2019-06-05 14:18:15 +0800

    优化url.encode与url.decode

--- luaclib/src/lsys.c
+++ luaclib/src/lsys.c
@@ -2,6 +2,10 @@
 
 #include "../../src/core.h"
 
+#define hex_char(ch) ({(uint8_t)((ch) > 9 ? (ch) + 55: (ch) + 48);})
+
+#define is_normal_char(ch) ({((ch) >= 'a' && (ch) <= 'z') || ((ch) >= 'A' && (ch) <= 'Z') || ((ch) >= '0' && (ch) <= '9') ? 1 : 0;})
+
 // 提供一个精确到毫秒的时间戳
 static int
 lnow(lua_State *L){
@@ -9,7 +13,7 @@ lnow(lua_State *L){
 	return 1;
 }
 
-int /* 此方法可用于检查是否为有效ipv4地址*/
+static int /* 此方法可用于检查是否为有效ipv4地址*/
 lipv4(lua_State *L){
     const char *IP = lua_tostring(L, 1);
     if (!IP) return luaL_error(L, "ipv4 error: 请至少传递一个string类型参数\n");
@@ -18,7 +22,7 @@ lipv4(lua_State *L){
     return 1;
 }
 
-int /* 此方法可用于检查是否为有效ipv6地址*/
+static int /* 此方法可用于检查是否为有效ipv6地址*/
 lipv6(lua_State *L){
     const char *IP = lua_tostring(L, 1);
     if (!IP) return luaL_error(L, "ipv6 error: 请至少传递一个string类型参数\n");
@@ -27,7 +31,7 @@ lipv6(lua_State *L){
     return 1;
 }
 
-int
+static int
 ldate(lua_State *L){
     const char *fmt = lua_tostring(L, 1);
     if (!fmt) return luaL_error(L, "Date: 错误的格式化方法");
@@ -39,16 +43,62 @@ ldate(lua_State *L){
     return 1;
 }
 
+static int /* url编码 */
+lurlencode(lua_State *L){
+	size_t url_len;
+  const char* url = luaL_checklstring(L, 1, &url_len);
+	if (!url) return luaL_error(L, "urlencode error: 需要传递一个有效的url字符串!");
+	luaL_Buffer convert_url;
+	luaL_buffinit(L, &convert_url);
+	while (*url) {
+		uint8_t ch = (uint8_t)*url++;
+		if (ch == ' ') {
+			luaL_addlstring(&convert_url, "%20", 3);
+			continue;
+		}
+		if (is_normal_char(ch) || strchr("-_.!~*'()", ch)){
+			luaL_addchar(&convert_url, ch);
+			continue;
+		}
+		char ver[3] = {'%', hex_char(((uint8_t)ch) >> 4), hex_char(((uint8_t)ch) & 15)};
+		luaL_addlstring(&convert_url, ver, 3);
+	}
+	luaL_pushresult(&convert_url);
+	return 1;
+}
+
+static int /* url解码 */
+lurldecode(lua_State *L){
+	size_t url_len, i = 0, j = 0;
+  const char* url = luaL_checklstring(L, 1, &url_len);
+	if (!url) return luaL_error(L, "urldecode error: 需要传递一个有效的url字符串!");
+	char convert_url[url_len];
+	while (i < url_len) {
+		if (url[i] != '%') {
+			convert_url[j++] = url[i++];
+			continue;
+		}
+		char vert[2] = {url[i + 1], url[i + 2]};
+		convert_url[j++] = (uint8_t)((vert[0] - 48 - ((vert[0] >= 'A') ? 7 : 0) - ((vert[0] >= 'a') ? 32 : 0)) * 16 + (vert[1] - 48 - ((vert[1] >= 'A') ? 7 : 0) - ((vert[1] >= 'a') ? 32 : 0)));
+		i += 3;
+	}
+	if (j < i) convert_url[j] = '\0';
+	lua_pushstring(L, convert_url);
+	return 1;
+}
+
 LUAMOD_API int
 luaopen_sys(lua_State *L){
-    luaL_checkversion(L);
-    luaL_Reg sys_libs[] = {
-        {"now", lnow},
-        {"ipv4", lipv4},
-        {"ipv6", lipv6},
-        {"date", ldate},
-        {NULL, NULL}
-    };
-    luaL_newlib(L, sys_libs);
-    return 1;
+	luaL_checkversion(L);
+	luaL_Reg sys_libs[] = {
+		{"now", lnow},
+		{"ipv4", lipv4},
+		{"ipv6", lipv6},
+		{"date", ldate},
+		{"urlencode", lurlencode},
+		{"urldecode", lurldecode},
+		{NULL, NULL}
+	};
+	luaL_newlib(L, sys_libs);
+	return 1;
 }
--- lualib/url/init.lua
+++ lualib/url/init.lua
@@ -1,19 +1,30 @@
-local tonumber = tonumber
-local byte = string.byte
-local char = string.char
-local fmt = string.format
-local spliter = string.gsub
+-- lua版实现
+-- local tonumber = tonumber
+-- local byte = string.byte
+-- local char = string.char
+-- local fmt = string.format
+-- local spliter = string.gsub
+
+-- C版实现
+local encode = require("sys").urlencode
+local decode = require("sys").urldecode
+
+--[[
+经过测试: 100万此编码/解码两者性能相差30倍, 正好是lua与C的性能差距.
+]]
 
 local url = {}
 
 -- urlencode编码
 function url.encode(s)
-    return spliter(spliter(s, "([^%w%.%- ])", function(c) return fmt("%%%02X", byte(c)) end), " ", "+")
+  -- return spliter(spliter(s, "([^%w%.%- ])", function(c) return fmt("%%%02X", byte(c)) end), " ", "+")
+  return encode(s)
 end
 
--- urlencode解码
+-- urldecode解码
 function url.decode(s)
-    return spliter(s, '%%(%x%x)', function(h) return char(tonumber(h, 16)) end)
+  -- return spliter(s, '%%(%x%x)', function(h) return char(tonumber(h, 16)) end)
+  return decode(s)
 end
 
 return url
