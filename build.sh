c33f3cd566b3844b1cf0d73d18fc8b55924cbcc7
CandyMi <869646063@qq.com>
2019-07-04 04:31:57 +0800

    更新构建脚本

--- build.sh
+++ build.sh
@@ -6,12 +6,12 @@ rm -rf build && mkdir build && cd build
 git clone https://github.com/CandyMi/lua -b v5.3.5
 git clone https://github.com/CandyMi/libev -b v4.25
 
-cd ${current}/build/lua &&
-  make all MYCFLAGS=-fPIC MYCFLAGS+=-DLUA_USE_DLOPEN SYSLIBS=-ldl &&
-  cp lua.h luaconf.h lualib.h lauxlib.h /usr/local/include && cp liblua.* /usr/local/lib
-
 cd ${current}/build/libev &&
   sh autogen.sh && ./configure --prefix=/usr/local &&
   make && make install
+
+cd ${current}/build/lua &&
+  make all MYCFLAGS=-fPIC MYCFLAGS+=-DLUA_USE_DLOPEN SYSLIBS=-ldl &&
+  cp lua.h luaconf.h lualib.h lauxlib.h /usr/local/include && cp liblua.* /usr/local/lib
 
 cd ${current} && rm -rf build
