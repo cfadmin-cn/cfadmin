828a5412d2bc68a616e1c6f3dd241ed1299c817e
CandyMi <869646063@qq.com>
2019-07-14 11:11:48 +0800

    更新core.c

--- src/core.c
+++ src/core.c
@@ -107,7 +107,7 @@ init_lua_libs(lua_State *L){
   lua_pushliteral(L, "lualib/?.lua;lualib/?/init.lua;./?.lua;./?/init.lua;script/?.lua;script/?/init.lua;");
   lua_setfield(L, 1, "path");
 
-  lua_pushliteral(L, "luaclib/?.so;luaclib/?/init.so;./?.so;./?/init.so;");
+  lua_pushliteral(L, "luaclib/?.so;luaclib/lib?.dylib;luaclib/?.dll;./?.so;./lib?.dylib;./?.dll");
   lua_setfield(L, 1, "cpath");
 
   lua_settop(L, 0);
