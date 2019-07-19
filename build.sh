63b48ec2b5446a353b1899f3a6fcd3fb84f59fb7
CandyMi <869646063@qq.com>
2019-07-19 13:14:33 +0800

    更新build.sh

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ cd ${current}/build/libev &&
   make && make install
 
 cd ${current}/build/lua &&
-  make all MYCFLAGS=-fPIC MYCFLAGS+=-DLUA_USE_DLOPEN SYSLIBS="-ldl -lreadline" &&
+  make all MYCFLAGS=-fPIC MYCFLAGS+=-DLUA_USE_POSIX MYCFLAGS+=-DLUA_USE_DLOPEN MYLIBS="-ldl -lreadline" &&
   cp lua.h luaconf.h lualib.h lauxlib.h /usr/local/include && cp liblua.* /usr/local/lib
 
 cd ${current} && rm -rf build
