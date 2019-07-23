98fff0d9001a40d6c0717c438720fc76b7f3484e
CandyMi <869646063@qq.com>
2019-07-24 07:18:44 +0800

    格式化修改

--- lualib/internal/TCP.lua
+++ lualib/internal/TCP.lua
@@ -267,93 +267,91 @@ function TCP:listen(ip, port, cb)
 end
 
 function TCP:connect(domain, port)
-    local ok, IP = dns_resolve(domain)
-    if not ok then
-        return nil, "Can't resolve this domain or ip:"..(domain or IP or "")
-    end
-    self.fd = tcp_new_client_fd(IP, port)
-    if not self.fd then
-        return nil, "Connect This host fault! "..(domain or "no domain")..":"..(port or "no port")
-    end
-    local co = co_self()
-    self.CONNECT_IO = tcp_pop()
-    self.connect_current_co = co_self()
-    self.connect_co = co_new(function (connected)
-        if self.timer then
-            self.timer:stop()
-            self.timer = nil
-        end
-        tcp_push(self.CONNECT_IO)
-        tcp_stop(self.CONNECT_IO)
-        self.connect_current_co = nil
-        self.CONNECT_IO = nil
-        self.connect_co = nil
-        if connected then
-            return co_wakeup(co, true)
-        end
-        return co_wakeup(co, false, '连接失败')
-    end)
-    self.timer = ti_timeout(self._timeout, function ( ... )
-        tcp_push(self.CONNECT_IO)
-        tcp_stop(self.CONNECT_IO)
+  local ok, IP = dns_resolve(domain)
+  if not ok then
+      return nil, "Can't resolve this domain or ip:"..(domain or IP or "")
+  end
+  self.fd = tcp_new_client_fd(IP, port)
+  if not self.fd then
+      return nil, "Connect This host fault! "..(domain or "no domain")..":"..(port or "no port")
+  end
+  local co = co_self()
+  self.CONNECT_IO = tcp_pop()
+  self.connect_current_co = co_self()
+  self.connect_co = co_new(function (connected)
+    if self.timer then
+        self.timer:stop()
         self.timer = nil
-        self.CONNECT_IO = nil
-        self.connect_co = nil
-        self.connect_current_co = nil
-        return co_wakeup(co, nil, 'connect timeot.')
-    end)
-    tcp_connect(self.CONNECT_IO, self.fd, self.connect_co)
-    return co_wait()
+    end
+    tcp_push(self.CONNECT_IO)
+    tcp_stop(self.CONNECT_IO)
+    self.connect_current_co = nil
+    self.CONNECT_IO = nil
+    self.connect_co = nil
+    if connected then
+        return co_wakeup(co, true)
+    end
+    return co_wakeup(co, false, '连接失败')
+  end)
+  self.timer = ti_timeout(self._timeout, function ( ... )
+      tcp_push(self.CONNECT_IO)
+      tcp_stop(self.CONNECT_IO)
+      self.timer = nil
+      self.CONNECT_IO = nil
+      self.connect_co = nil
+      self.connect_current_co = nil
+      return co_wakeup(co, nil, 'connect timeot.')
+  end)
+  tcp_connect(self.CONNECT_IO, self.fd, self.connect_co)
+  return co_wait()
 end
 
 function TCP:ssl_connect(domain, port)
-    local ok, err = self:connect(domain, port)
-    if not ok then
-        return nil, "SSL connect error."
-    end
-    self.ssl_ctx, self.ssl = tcp.new_ssl(self.fd)
-    if not self.ssl_ctx or not self.ssl then
-        return Log:ERROR("Create a SSL Error! :) ")
-    end
-
-    local co = co_self()
-    self.CONNECT_IO = tcp_pop()
-    self.connect_current_co = co
-    self.connect_co = co_new(function ()
-      local EVENTS = EVENT_WRITE
-      while 1 do
-          local ok, EVENT = tcp_ssl_do_handshak(self.ssl)
-          if ok or not EVENT then
-            if self.timer then
-              self.timer:stop()
-              self.timer = nil
-            end
-            tcp_push(self.CONNECT_IO)
-            tcp_stop(self.CONNECT_IO)
-            self.CONNECT_IO = nil
-            self.connect_co = nil
-            self.connect_current_co = nil
-            return co_wakeup(co, ok)
-          end
-          if EVENTS ~= EVENT then
-            EVENTS = EVENT
-            tcp_stop(self.CONNECT_IO)
-            tcp_start(self.CONNECT_IO, self.fd, EVENTS, self.connect_co)
-          end
-          co_wait()
-      end
-    end)
-    self.timer = ti_timeout(self._timeout, function ( ... )
+  local ok, err = self:connect(domain, port)
+  if not ok then
+      return nil, "domain connect error."
+  end
+  self.ssl_ctx, self.ssl = tcp.new_ssl(self.fd)
+  if not self.ssl_ctx or not self.ssl then
+      return nil, "create a ssl ctx error."
+  end
+  self.CONNECT_IO = tcp_pop()
+  self.connect_current_co = co_self()
+  self.connect_co = co_new(function ()
+    local EVENTS = EVENT_WRITE
+    while 1 do
+      local ok, EVENT = tcp_ssl_do_handshak(self.ssl)
+      if ok or not EVENT then
+        if self.timer then
+          self.timer:stop()
+          self.timer = nil
+        end
         tcp_push(self.CONNECT_IO)
         tcp_stop(self.CONNECT_IO)
-        self.timer = nil
         self.CONNECT_IO = nil
         self.connect_co = nil
         self.connect_current_co = nil
-        return co_wakeup(co, nil, 'ssl_connect timeot.')
-    end)
-    tcp_start(self.CONNECT_IO, self.fd, EVENT_WRITE, self.connect_co)
-    return co_wait()
+        return co_wakeup(co, ok)
+      end
+      if EVENTS ~= EVENT then
+        EVENTS = EVENT
+        tcp_stop(self.CONNECT_IO)
+        tcp_start(self.CONNECT_IO, self.fd, EVENTS, self.connect_co)
+      end
+      co_wait()
+    end
+  end)
+  self.timer = ti_timeout(self._timeout, function ( ... )
+      tcp_push(self.CONNECT_IO)
+      tcp_stop(self.CONNECT_IO)
+      self.timer = nil
+      self.CONNECT_IO = nil
+      self.connect_co = nil
+      self.connect_current_co = nil
+      return co_wakeup(co, nil, 'ssl_connect timeot.')
+  end)
+  tcp_start(self.CONNECT_IO, self.fd, EVENT_WRITE, self.connect_co)
+  return co_wait()
 end
 
 function TCP:count()
