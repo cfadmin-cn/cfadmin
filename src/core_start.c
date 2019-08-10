f3a59b9b2c5c7a61eab9fdaeae1608e941dcbcdf
CandyMi <869646063@qq.com>
2019-08-11 03:31:26 +0800

    在根目录增加pid文件

--- src/core_start.c
+++ src/core_start.c
@@ -1,16 +1,33 @@
 #include "core.h"
 
+void
+write_pid(const char *filename) {
+  errno = 0;
+  FILE *f = fopen(filename, "w");
+  if (!f) {
+    LOG("ERROR", strerror(errno));
+    return exit(-1);
+  }
+  fprintf(f, "%d\n", getpid());
+  fclose(f);
+}
+
 int
 main(int argc, char const *argv[])
 {
 // #if !defined(__APPLE__)
-    /* 后台运行 */
-	if (argc > 1 && 0 == strcmp("-d", argv[argc-1])) daemon(1, 0);
+  /* 后台运行 */
+  if (argc > 1 && 0 == strcmp("-d", argv[argc-1])) daemon(1, 0);
 // #endif
-	/* 系统初始化 */
-	core_sys_init();
-    /* 运行事件循环 */
-	core_sys_run();
 
-	return 0;
+  /* 建立Pid文件 */
+  write_pid("cfadmin.pid");
+
+  /* 系统初始化 */
+  core_sys_init();
+
+  /* 运行事件循环 */
+  core_sys_run();
+
+  return 0;
 }
