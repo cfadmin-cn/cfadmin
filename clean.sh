d211bbdaba458c3a47517e82e05373376fc7165a
CandyMi <869646063@qq.com>
2019-08-13 12:24:17 +0800

    更新清理编译库脚本并且增加清理说明

--- clean.sh
+++ clean.sh
@@ -1,3 +1,5 @@
+# This script is used to clean up build.sh to install the header files and library files brought by libev and lua. You can ignore this file when you already have lua and libev in your environment.
+# 此脚本为清理build.sh安装libev与lua带来的头文件与库文件, 当您的环境中已经有了lua与libev后可以忽略此文件.
 
 rm -rf libev* liblua*
 rm -rf src/l* src/ev*
