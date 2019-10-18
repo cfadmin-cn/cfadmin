daa7e3c4ed39b6e8342230b6852de9b631989f4e
CandyMi <869646063@qq.com>
2019-10-18 08:04:35 +0800

    test_upload增加七牛SDK上传示例

--- script/test_upload.lua
+++ script/test_upload.lua
@@ -3,23 +3,25 @@ local json = require "json"
 
 local app = httpd:new("httpd")
 
---[[
+app:use("/upload", function(content)
+  return [[
 <!DOCTYPE html>
 <html>
 <head>
     <title>本地文件上传</title>
 </head>
 <body>
-    <form action="http://localhost:8080/upload_local" method=post enctype="multipart/form-data">
-        <input type="file" name="点击上传" value="上传文件①" />
-            <br>
-        <input type="file" name="点击上传" value="上传文件②" />
-            <br>
-        <input type="submit" value="提交" />
-    </form>
+  <form action="http://localhost:8080/upload_local" method=post enctype="multipart/form-data">
+    <input type="file" name="点击上传" value="上传文件①" />
+        <br>
+    <input type="file" name="点击上传" value="上传文件②" />
+        <br>
+    <input type="submit" value="提交" />
+  </form>
 </body>
 </html>
 ]]
+end)
 
 app:api("/upload_local", function (content)
   if content.files then
@@ -38,7 +40,46 @@ app:api("/upload_local", function (content)
   return json.encode { code = 404, status = "没有上传内容." }
 end)
 
-app:static("static", 30)
+
+app:use('/qiniu_upload', function(content)
+  return [[
+<!DOCTYPE html>
+<html>
+<head>
+  <title>七牛文件上传</title>
+  <script type="text/javascript" src="/js/jquery.min.js"></script>
+</head>
+<body>
+  <form action="https://up-z2.qiniup.com" method=post enctype="multipart/form-data">
+    <input id="token" name="token" type="hidden">
+    <input name="file" type="file" />
+    <input type="submit" value="提交" />
+  </form>
+  <p id="print">无token</p>
+</body>
+<script type="text/javascript">
+  $.get("http://localhost:8080/qiniu_token", {},  function(data, status) {
+    $("#print").text(data.token)            // 将服务端生成的token打印出来.
+    $("#token").attr("value", data.token)   // 将服务端生成的token添加到token的value中.
+  })
+</script>
+</html>
+]]
+end)
+
+local oss = require "cloud.qiniu.oss"
+app:api('/qiniu_token', function (content)
+  local access_key = "your_access_key"
+  local secret_key = "your_secret_key"
+  return json.encode {
+    code = 200,
+    token = oss.getUploadToken(access_key, secret_key, {
+      bucket = "candymi", -- 对应bucket名称(这个必填, 其它选填)
+    }),
+  }
+end)
+
+app:static("static")
 
 app:listen("0.0.0.0", 8080)
 
