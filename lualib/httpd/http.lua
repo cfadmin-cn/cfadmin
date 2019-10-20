39fdec90749ce07156c0e777adc00e048f545e12
CandyMi <869646063@qq.com>
2019-10-21 05:12:35 +0800

    httpd.http实现增加jwt与basic生成方法, 使用方式请直接参考此文件

--- lualib/httpd/http.lua
+++ lualib/httpd/http.lua
@@ -1,6 +1,15 @@
+local json = require "json"
+local json_encode = json.encode
+
+local crypt = require "crypt"
+local hmac_sha256 = crypt.hmac_sha256
+local base64encode = crypt.base64encode
+local base64urlencode = crypt.base64urlencode
+
 local type = type
 local assert = assert
 local find = string.find
+local concat = table.concat
 
 local http = {}
 --[[
@@ -12,6 +21,24 @@ local http = {}
 -- 成功
 function http.ok()
 	return 200
+end
+
+-- http base authorization
+function http.basic_authorization(username, password)
+	return "Authorization", "Basic " .. base64encode(username .. ":" .. password)
+end
+
+-- Json Web Token
+function http.jwt(secret, payload)
+	local content = {nil, nil, nil}
+	-- header
+	content[#content + 1] = base64urlencode(json_encode{ alg = "HS256", typ = "JWT" })
+	-- payload
+	content[#content + 1] = base64urlencode(payload)
+	-- signature
+	content[#content + 1] = hmac_sha256(secret, concat(content, "."), true)
+	-- result.
+	return "Authorization", "Bearer " .. concat(content, ".")
 end
 
 -- 重定向
