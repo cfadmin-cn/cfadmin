bba0d4d7b56bde1a60ea1b9e13e71a025b014563
CandyMi <869646063@qq.com>
2019-11-03 14:38:23 +0800

    优化httpd路由查找

--- lualib/httpd/Router.lua
+++ lualib/httpd/Router.lua
@@ -6,7 +6,6 @@ local url_decode = url.decode
 
 local new_tab = require("sys").new_tab
 
-local math = math
 local string = string
 local split = string.sub
 local find = string.find
@@ -16,13 +15,10 @@ local spliter = string.gsub
 
 local type = type
 local next = next
-local assert = assert
 local ipairs = ipairs
 local tonumber = tonumber
 local tostring = tostring
 local io_open = io.open
-local concat = table.concat
-local toint = math.tointeger
 
 local Router = {
 	API = 1,
@@ -35,19 +31,18 @@ local routes = {} -- 存储路由
 
 local static = {} -- 静态文件路由
 
-local typ = {
-	int = toint,
-	float = tonumber,
-	string = tostring,
-}
-
--- 分割路径后进行hex, 得到key后一次查表即可完成
+-- 主要用作分割路径判断.
 local function hex_route(route)
 	local tab = new_tab(32, 0)
 	for r in splite(route, '/([^/%?]+)') do
 		tab[#tab + 1] = r
 	end
 	return tab
+end
+
+-- 主要用作分割hash路由查找
+local function to_route(route)
+	return spliter(route, "/", "")
 end
 
 -- 检查是路径回退是否超出静态文件根目录
@@ -94,46 +89,48 @@ end
 local load_file
 
 local function registery_router (route, class, route_type)
-	routes[concat(hex_route(route))] = {class = class, type = route_type}
+	routes[to_route(route)] = {class = class, type = route_type}
 end
 
 local function find_route (method, path)
-	local tab = hex_route(split(path, 1, (find(path, '?') or 0) - 1))
-	local hex = concat(tab)
-	local t = routes[hex]
-	if t then
-		return t.class, t.type
-	end
-	local prefix, type = static.prefix, static.type
-	if not prefix and not type then
-		return
-	end
-	-- 非GET/HEAD方法不查找静态文件
-	if method ~= 'GET' and method ~= 'HEAD' then
-		return
-	end
-	-- 凡是找到'../'并且检查路径回退已经超出静态文件根目录返回404
-	if find(path, '/../', 1, true) and check_path_deep(tab) then
-		return
-	end
-	load_file = load_file or function (path)
-		local filepath = prefix..url_decode(path)
-		-- 使用r+测试是否可读可写; 如果filepath是目录则无法被打开, 但单独的r模式可以.
-		local f, error = io_open(filepath, 'r+')
-		if not f then
-			return
-		end
-		local body_len = f:seek("end")
-		f:close()
-		return body_len, filepath, match(path, '.+%.([%a]+)')
-	end
-	return load_file, type
+  path = split(path, 1, (find(path, '?') or 0) - 1)
+	local t = routes[to_route(path)]
+  if t then
+    return t.class, t.type
+  end
+	local prefix, typ = static.prefix, static.type
+	if not prefix and not typ then
+    return
+  end
+  -- 非GET/HEAD方法不查找静态文件
+  if method ~= 'GET' and method ~= 'HEAD' then
+    return
+  end
+  local tab = hex_route(path)
+  -- 凡是找到'../'并且检查路径回退已经超出静态文件根目录返回404
+  if find(path, '/../', 1, true) and check_path_deep(tab) then
+    return
+  end
+  if not load_file then
+		load_file = function (path)
+      local filepath = prefix .. url_decode(path)
+      -- 使用r+测试是否可读可写; 如果filepath是目录则无法被打开, 但单独的r模式可以.
+      local f, error = io_open(filepath, 'r+')
+      if not f then
+        return
+      end
+      local body_len = f:seek("end")
+      f:close()
+      return body_len, filepath, match(path, '.+%.([%a]+)')
+    end
+  end
+  return load_file, typ
 end
 
 -- 查找路由
 function Router.find(method, path)
 	-- 凡是不以'/'开头的path都返回404
-	if not find(path, '^(/)') then
+	if not find(path, '^/') then
 		return
 	end
 	return find_route(method, path)
