5062d1fff0fa6f7e56d66eb4bf7dce875dfe37ee
CandyMi <869646063@qq.com>
2019-11-07 02:11:15 +0800

    修复一个class引用的问题

--- lualib/class/init.lua
+++ lualib/class/init.lua
@@ -1,30 +1,30 @@
 local type = type
 local setmetatable = setmetatable
--- 一个精简版的类实现
-function class(cls_name)
-    local cls = { __name = cls_name }
-    cls.__index = cls
-    cls.__call = function (cls, ...)
-        local call = cls[cls_name]
-        if call then
-            return call(cls, ...)
-        end
-        return
-    end
-    cls.new = function (c, ...)
-        if cls ~= c then
-            return print("Please use ':' to create new object :)")
-        end
-        local t = {}
-        local ctor = c.ctor
-        if type(ctor) ~= 'function' then
-            print("Can't find ctor to init.")
-        else
-            ctor(t, ...)
-        end
-        return setmetatable(t, cls)
-    end
-    return cls
-end
+local new_tab = require "sys".new_tab
 
-return class
+-- 一个精简版的类实现
+return function (cls_name)
+  local cls = {}
+  cls.__name = cls_name
+  cls.__index = cls
+  cls.__call = function (c, ...)
+    local call = c[cls_name]
+    if type(call) ~= 'function' then
+      return
+    end
+    return call(c, ...)
+  end
+  cls.new = function (c, ...)
+    if cls ~= c then
+      return print("Please use ':' to create new object :)")
+    end
+    local obj = new_tab(0, 16)
+    local ctor = cls.ctor
+    if not ctor then
+      return print("Can't find ctor to init.")
+    end
+    ctor(obj, ...)
+    return setmetatable(obj, cls)
+  end
+  return cls
+end
