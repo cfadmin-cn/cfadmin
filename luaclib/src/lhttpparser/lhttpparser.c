6535ebf587487df5fb373948d6a08f7d47444663
CandyMi <869646063@qq.com>
2019-11-08 11:18:36 +0800

    优化http_parser的解析代码

--- luaclib/src/lhttpparser/lhttpparser.c
+++ luaclib/src/lhttpparser/lhttpparser.c
@@ -7,57 +7,53 @@ static int
 lparser_response_chunked(lua_State *L){
   size_t buf_len;
   const char* data = luaL_checklstring(L, 1, &buf_len);
-  char* buf = xmalloc(buf_len);
-  if (!buf){
-    LOG("ERROR", "Alloca memory falt.");
-    return 0;
-  }
-  memmove(buf, data, buf_len);
+
+  luaL_Buffer B;
+  char *buf = luaL_buffinitsize(L, &B, buf_len);
+  luaL_addlstring(&B, data, buf_len);
 
   struct phr_chunked_decoder decoder = { .consume_trailer = 1 };
-
   int last = phr_decode_chunked(&decoder, buf, &buf_len);
   if (0 > last) {
+    luaL_pushresultsize(&B, 0);
     lua_pushnil(L);
     lua_pushinteger(L, last);
-    xfree(buf);
     return 2;
   }
   lua_pushlstring(L, buf, buf_len);
-  xfree(buf);
   return 1;
 }
 
 static int
 lparser_http_request(lua_State *L){
-    size_t buf_len;
-    const char* buf = luaL_checklstring(L, 1, &buf_len);
+  size_t buf_len;
+  const char* buf = luaL_checklstring(L, 1, &buf_len);
 
-    int minor_version, ret, i;
-    const char *method;
-    const char *path;
-    size_t method_len, path_len, num_headers;
+  int minor_version, ret, i;
+  const char *method;
+  const char *path;
+  size_t method_len, path_len, num_headers;
 
-    struct phr_header headers[128];
-    memset(headers, 0x0, sizeof(headers));
+  struct phr_header headers[128];
+  memset(headers, 0x0, sizeof(headers));
 
-    num_headers = sizeof(headers) / sizeof(headers[0]);
-    ret = phr_parse_request(buf, buf_len, &method, &method_len, &path, &path_len, &minor_version, headers, &num_headers, 0);
-    if (0 > ret) return 0;
+  num_headers = sizeof(headers) / sizeof(headers[0]);
+  ret = phr_parse_request(buf, buf_len, &method, &method_len, &path, &path_len, &minor_version, headers, &num_headers, 0);
+  if (0 > ret) return 0;
 
-    lua_pushlstring(L, method, method_len); // METHOD
-    lua_pushlstring(L, path, path_len);  // PATH
-    lua_pushnumber(L, minor_version > 0 ? 1.1 : 1.0); // VERSION
+  lua_pushlstring(L, method, method_len); // METHOD
+  lua_pushlstring(L, path, path_len);  // PATH
+  lua_pushnumber(L, minor_version > 0 ? 1.1 : 1.0); // VERSION
 
-    lua_createtable(L, 0, 128);
-    for (i = 0; i < 128; i++){
-      if (!headers[i].name || !headers[i].value)
-        break;
-      lua_pushlstring(L, headers[i].name, headers[i].name_len);
-      lua_pushlstring(L, headers[i].value, headers[i].value_len);
-      lua_rawset(L, lua_gettop(L) - 2);
-    }
-    return 4;
+  lua_createtable(L, 0, 128);
+  for (i = 0; i < 128; i++){
+    if (!headers[i].name || !headers[i].value)
+      break;
+    lua_pushlstring(L, headers[i].name, headers[i].name_len);
+    lua_pushlstring(L, headers[i].value, headers[i].value_len);
+    lua_rawset(L, lua_gettop(L) - 2);
+  }
+  return 4;
 }
 
 static int
