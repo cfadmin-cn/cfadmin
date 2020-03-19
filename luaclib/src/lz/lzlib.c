de95af1d84185e31c6e89b209a74e076ff24b66b
869646063@qq.com <869646063@qq.com>
2020-03-19 16:42:33 +0800

    增加compress/uncompress2用来实现deflate压缩

--- luaclib/src/lz/lzlib.c
+++ luaclib/src/lz/lzlib.c
@@ -24,7 +24,7 @@ static int lcompress(lua_State *L) {
     return 0;
 
   lua_pushlstring(L, (const char*)out, out_size);
-  lua_pushinteger(L, out_size);
+  lua_pushinteger(L, in_size);
   return 2;
 }
 
@@ -35,10 +35,17 @@ static int luncompress(lua_State *L) {
     return 0;
 
   size_t out_size = in_size;
+
+  /* 若能传递压缩前的大小, 优先使用此数值 */
+  int is_sum = 0;
+  lua_Integer before_size = lua_tointegerx(L, 2, &is_sum);
+  if (is_sum && before_size > 0 )
+    out_size = before_size;
+
   size_t offset = 1;
   size_t top = lua_gettop(L);
 
-  for(;;) {
+  do {
     uint8_t *out = lua_newuserdata(L, out_size);
     memset(out, 0x0, out_size);
 
@@ -53,8 +60,7 @@ static int luncompress(lua_State *L) {
       out_size = in_size << offset;
       continue;
     }
-    break;
-  }
+  } while(0);
   return 0;
 }
 
@@ -67,7 +73,7 @@ static int lgzip_compress(lua_State *L) {
   z_stream z;
   stream_init(&z);
 
-  int ok = deflateInit2(&z, Z_DEFAULT_COMPRESSION, Z_DEFLATED, MAX_WBITS + 16, 8, Z_DEFAULT_STRATEGY);
+  int ok = deflateInit2(&z, Z_DEFAULT_COMPRESSION, Z_DEFLATED, MAX_WBITS + 16, MAX_MEM_LEVEL, Z_DEFAULT_STRATEGY);
   if (Z_OK != ok)
     return 0;
 
@@ -130,8 +136,101 @@ static int lgzip_uncompress(lua_State *L) {
       inflateEnd(&z);
       return 0;
     }
+    /* 防止内存溢出 */
+    if (out_size > in_size * 256){
+      luaL_pushresultsize(&B, 0);
+      inflateEnd(&z);
+      return 0;
+    }
     inflateReset(&z);
     out_size <<= 1;
+    luaL_pushresultsize(&B, 0);
+    lua_settop(L, top);
+  }
+  return 1;
+}
+
+static int lcompress2(lua_State *L) {
+  size_t in_size = 0;
+  const char* in = luaL_checklstring(L, 1, &in_size);
+  if (in_size <= 0)
+    return 0;
+
+  z_stream z;
+  stream_init(&z);
+
+  int ok = deflateInit2(&z, Z_DEFAULT_COMPRESSION, Z_DEFLATED, MAX_WBITS * -1, MAX_MEM_LEVEL, Z_DEFAULT_STRATEGY);
+  if (Z_OK != ok)
+    return 0;
+
+  z.next_in = (uint8_t *)in;
+  z.avail_in = in_size;
+
+  luaL_Buffer B;
+  z.avail_out = deflateBound(&z, in_size);
+  z.next_out = (uint8_t *)luaL_buffinitsize(L, &B, z.avail_out);
+  memset(z.next_out, 0x0, z.avail_out);
+
+  int ret = deflate(&z, Z_FINISH);
+  if (ret != Z_STREAM_END) {
+    luaL_pushresultsize(&B, 0);
+    deflateEnd(&z);
+    return 0;
+  }
+  if (deflateEnd(&z) != Z_OK){
+    luaL_pushresultsize(&B, 0);
+    return 0;
+  }
+  luaL_pushresultsize(&B, z.total_out);
+  lua_pushinteger(L, in_size);
+  return 2;
+}
+
+static int luncompress2(lua_State *L) {
+  size_t in_size = 0;
+  const char* in = luaL_checklstring(L, 1, &in_size);
+  if (in_size <= 0)
+    return 0;
+
+  z_stream z;
+  stream_init(&z);
+
+  int ok = inflateInit2(&z, MAX_WBITS * -1);
+  if (Z_OK != ok)
+    return 0;
+
+  size_t out_size = in_size * 4;
+  int top = lua_gettop(L);
+
+  for(;;) {
+    luaL_Buffer B;
+    z.next_in = (uint8_t *)in;
+    z.avail_in = in_size;
+    z.avail_out = out_size;
+    z.next_out = (uint8_t *)luaL_buffinitsize(L, &B, out_size);
+    memset(z.next_out, 0x0, z.avail_out);
+
+    int ret = inflate(&z, Z_FINISH);
+    if (ret == Z_STREAM_END) {
+      int ok = inflateEnd(&z);
+      if (ok != Z_OK)
+        return 0;
+      luaL_pushresultsize(&B, z.total_out);
+      break;
+    }
+    if (ret != Z_BUF_ERROR) {
+      luaL_pushresultsize(&B, 0);
+      inflateEnd(&z);
+      return 0;
+    }
+    /* 防止内存溢出 */
+    if (out_size > in_size * 256){
+      luaL_pushresultsize(&B, 0);
+      inflateEnd(&z);
+      return 0;
+    }
+    inflateReset(&z);
+    out_size *= 2;
     luaL_pushresultsize(&B, 0);
     lua_settop(L, top);
   }
@@ -144,6 +243,9 @@ LUAMOD_API int luaopen_lz(lua_State *L){
     /* LZ77压缩/解压方法 */
     {"compress", lcompress},
     {"uncompress", luncompress},
+    /* 原生压缩方法 */
+    {"compress2", lcompress2},
+    {"uncompress2", luncompress2},
     /* gzip压缩/解压方法 */
     {"gzcompress", lgzip_compress},
     {"gzuncompress", lgzip_uncompress},
