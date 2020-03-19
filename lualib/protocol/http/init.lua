7f1604ceadbadfdda2a39f4f28eb2da5557a48b8
869646063@qq.com <869646063@qq.com>
2020-03-20 01:09:13 +0800

    补充websocket的'x-webkit-deflate-frame'兼容支持

--- lualib/protocol/http/init.lua
+++ lualib/protocol/http/init.lua
@@ -242,9 +242,15 @@ local function Switch_Protocol(http, cls, sock, header, method, version, path, i
     response[#response+1] = "Sec-Websocket-Protocol: "..tostring(protocol)
   end
   local ext = nil
-  if type(header['Sec-WebSocket-Extensions']) == 'string' and find(header['Sec-WebSocket-Extensions'], "permessage%-deflate") then
-    response[#response+1] = "Sec-WebSocket-Extensions: permessage-deflate; server_no_context_takeover; client_no_context_takeover"
-    ext = "deflate"
+  local extension = header['Sec-WebSocket-Extensions']
+  if type(extension) == 'string' and extension ~= '' then
+    if find(header['Sec-WebSocket-Extensions'], "permessage%-deflate") then
+      response[#response+1] = "Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits=15; server_no_context_takeover; client_no_context_takeover"
+      ext = "deflate"
+    elseif find(header['Sec-WebSocket-Extensions'], "x%-webkit%-deflate%-frame") then
+      response[#response+1] = "Sec-WebSocket-Extensions: x-webkit-deflate-frame; no_context_takeover"
+      ext = "deflate"
+    end
   end
   -- require "utils"
   -- var_dump(header)
