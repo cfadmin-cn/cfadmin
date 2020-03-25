f6e14a01137dad9f144ec62134c6c352b31424e2
869646063@qq.com <869646063@qq.com>
2020-03-25 12:58:14 +0800

    修复uuid.c警告

--- luaclib/src/lcrypt/uuid.c
+++ luaclib/src/lcrypt/uuid.c
@@ -21,7 +21,7 @@ static inline int uuid_v4_gen(char *buffer)
   uuid.clk_seq_hi_res = (uint8_t) ((uuid.clk_seq_hi_res & 0x3F) | 0x80);
   uuid.time_hi_and_version = (uint16_t) ((uuid.time_hi_and_version & 0x0FFF) | 0x4000);
 
-  snprintf(buffer, UUID_V4_LENGTH, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
+  snprintf(buffer, UUID_V4_LENGTH + 1, "%08x-%04x-%04x-%02x%02x-%02x%02x%02x%02x%02x%02x",
     uuid.time_low, uuid.time_mid, uuid.time_hi_and_version,
     uuid.clk_seq_hi_res, uuid.clk_seq_low,
     uuid.node[0], uuid.node[1], uuid.node[2],
@@ -32,9 +32,8 @@ static inline int uuid_v4_gen(char *buffer)
 }
 
 int luuid(lua_State *L) {
-  luaL_Buffer b;
-  char *UUID = luaL_buffinitsize(L, &b, UUID_V4_LENGTH);
+  char* UUID = lua_newuserdata(L, UUID_V4_LENGTH + 1);
   uuid_v4_gen(UUID);
-  luaL_pushresultsize(&b, UUID_V4_LENGTH);
+  lua_pushlstring(L, UUID, UUID_V4_LENGTH);
   return 1;
 }
