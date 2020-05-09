948ef11beb7fd60c0442862ee4900db8cef18f05
869646063@qq.com <869646063@qq.com>
2020-05-09 16:07:44 +0800

    适配更多写法的路由注册与查找

--- lualib/httpd/Router.lua
+++ lualib/httpd/Router.lua
@@ -54,7 +54,12 @@ end
 
 -- 主要用作分割hash路由查找
 local function to_route(route)
-	return spliter(route, "([/]+)", '/')
+  local r = spliter(route, "([/]+)", '/')
+  if byte(r, #r) ~= byte(slash) then
+    return r
+  end
+  print("删除: 结尾处的[/]")
+  return split(r, 1, -2)
 end
 
 -- 检查是路径回退是否超出静态文件根目录
