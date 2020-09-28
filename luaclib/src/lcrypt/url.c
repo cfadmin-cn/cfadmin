8d26fe2373678510fb5c82a73b1acaf95f15e751
Mr.che <869646063@qq.com>
2020-09-28 16:12:47 +0800

    Update url.c
    增加urldecode遇到加号('+')会自动转换为空格(' ')

--- luaclib/src/lcrypt/url.c
+++ luaclib/src/lcrypt/url.c
@@ -7,25 +7,25 @@
 /* url编码 */
 int lurlencode(lua_State *L){
 	size_t url_len;
-  const char* url = luaL_checklstring(L, 1, &url_len);
+	const char* url = luaL_checklstring(L, 1, &url_len);
 	if (!url)
 		return luaL_error(L, "Invalid url text");
 
 	luaL_Buffer convert_url;
 	luaL_buffinit(L, &convert_url);
 
-	while (*url) {
-		uint8_t ch = (uint8_t)*url++;
-		if (ch == ' ') {
+	for (int index = 0; index < url_len;) {
+		uint8_t ch = (uint8_t)url[index++];
+		if (ch == (uint8_t)' ') {
 			luaL_addlstring(&convert_url, "%20", 3);
 			continue;
 		}
-		if (is_normal_char(ch) || strchr("-_.!~*'()", ch)){
+		if (is_normal_char(ch) || strchr("-_.!~*'()", ch)) {
 			luaL_addchar(&convert_url, ch);
 			continue;
 		}
-		char ver[3] = {'%', hex_char(((uint8_t)ch) >> 4), hex_char(((uint8_t)ch) & 15)};
-		luaL_addlstring(&convert_url, ver, 3);
+		char vert[3] = {'%', hex_char(((uint8_t)ch) >> 4), hex_char(((uint8_t)ch) & 15)};
+		luaL_addlstring(&convert_url, vert, 3);
 	}
 
 	luaL_pushresult(&convert_url);
@@ -35,25 +35,23 @@ int lurlencode(lua_State *L){
 /* url解码 */
 int lurldecode(lua_State *L){
 	size_t url_len;
-  const char* url = luaL_checklstring(L, 1, &url_len);
+  	const char* url = luaL_checklstring(L, 1, &url_len);
 	if (!url)
 		return luaL_error(L, "Invalid url text");
 
 	luaL_Buffer convert_url;
 	luaL_buffinit(L, &convert_url);
 
-	while (*url) {
-		uint8_t ch = (uint8_t)*url++;
-		if (ch != '%') {
-			luaL_addchar(&convert_url, ch);
+	for (int index = 0; index < url_len;) {
+		uint8_t ch = (uint8_t)url[index++];
+		if (ch != (uint8_t)'%') {
+			luaL_addchar(&convert_url, ch == (uint8_t)'+' ? (uint8_t)' ' : ch);
 			continue;
 		}
-		char vert[2];
-		vert[0] = (uint8_t)*url++;
-		vert[1] = (uint8_t)*url++;
+		uint8_t vert[] = { (uint8_t)url[index++], (uint8_t)url[index++] };
 		luaL_addchar(&convert_url, (uint8_t)((vert[0] - 48 - ((vert[0] >= 'A') ? 7 : 0) - ((vert[0] >= 'a') ? 32 : 0)) * 16 + (vert[1] - 48 - ((vert[1] >= 'A') ? 7 : 0) - ((vert[1] >= 'a') ? 32 : 0))));
 	}
-	
+
 	luaL_pushresult(&convert_url);
 	return 1;
 }
