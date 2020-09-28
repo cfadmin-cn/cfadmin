b5d5c62dee3d58937077f839683efc73340cdfcd
869646063@qq.com <869646063@qq.com>
2020-09-28 22:52:35 +0800

    修复urlencode/urldecode在不同平台下的实现差异与编码行为问题.

--- luaclib/src/lcrypt/url.c
+++ luaclib/src/lcrypt/url.c
@@ -6,7 +6,7 @@
 
 /* url编码 */
 int lurlencode(lua_State *L){
-	size_t url_len;
+	size_t url_len = 0;
 	const char* url = luaL_checklstring(L, 1, &url_len);
 	if (!url)
 		return luaL_error(L, "Invalid url text");
@@ -35,8 +35,8 @@ int lurlencode(lua_State *L){
 
 /* url解码 */
 int lurldecode(lua_State *L){
-	size_t url_len;
-  	const char* url = luaL_checklstring(L, 1, &url_len);
+	size_t url_len = 0;
+	const char* url = luaL_checklstring(L, 1, &url_len);
 	if (!url)
 		return luaL_error(L, "Invalid url text");
 
@@ -45,12 +45,25 @@ int lurldecode(lua_State *L){
 
 	int index;
 	for (index = 0; index < url_len;) {
+
 		uint8_t ch = (uint8_t)url[index++];
 		if (ch != (uint8_t)'%') {
 			luaL_addchar(&convert_url, ch == (uint8_t)'+' ? (uint8_t)' ' : ch);
 			continue;
 		}
-		char vert[] = { (uint8_t)url[index++], (uint8_t)url[index++] };
+
+		char vert[2];
+		if (index++ == url_len) {
+			luaL_addchar(&convert_url, '%');
+			break;
+		}
+		vert[0] = url[index - 1];
+
+		if (index++ == url_len) {
+			luaL_addlstring(&convert_url, url + url_len - 2, 2);
+			break;
+		}
+		vert[1] = url[index - 1];
 		luaL_addchar(&convert_url, (uint8_t)((vert[0] - 48 - ((vert[0] >= 'A') ? 7 : 0) - ((vert[0] >= 'a') ? 32 : 0)) * 16 + (vert[1] - 48 - ((vert[1] >= 'A') ? 7 : 0) - ((vert[1] >= 'a') ? 32 : 0))));
 	}
 
