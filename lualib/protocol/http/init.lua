6d87d18baa30b355193315ced8a26e7173f74538
869646063@qq.com <869646063@qq.com>
2020-12-07 21:35:05 +0800

    修复代理服务器可能导致的header判断失败的问题

--- lualib/protocol/http/init.lua
+++ lualib/protocol/http/init.lua
@@ -175,9 +175,9 @@ local function PASER_METHOD(http, sock, max_body_size, buffer, METHOD, PATH, HEA
       local XML_ENCODE_2  = 'application/xml'
       local JSON_ENCODE = 'application/json'
       local URL_ENCODE  = 'application/x-www-form-urlencoded'
-      local format = match(HEADER['Content-type'] or HEADER['Content-Type'] or HEADER['content-type'] or '', '(.-/[^;]*)')
+      local format = match(HEADER['Content-Type'] or HEADER['content-type'] or '', '(.-/[^;]*)')
       if format == FILE_ENCODE then
-        local BOUNDARY = match(HEADER['Content-Type'], '^.+=[%-]*(.+)')
+        local BOUNDARY = match(HEADER['Content-Type'] or HEADER['content-type'] or '', '^.+=[%-]*(.+)')
         if BOUNDARY and BOUNDARY ~= '' then
           local typ, body = form_multipart(BODY, BOUNDARY)
           if typ == FILE_TYPE then
