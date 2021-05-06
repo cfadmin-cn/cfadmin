70e996167c864b315e9d3f311485893a065e2e1e
869646063@qq.com <869646063@qq.com>
2021-05-06 20:44:35 +0800

    优化Timer代码与注释

--- lualib/internal/Timer.lua
+++ lualib/internal/Timer.lua
@@ -16,93 +16,115 @@ local co_self = co.self
 
 local co_wait_ex = coroutine.yield
 
-local remove = table.remove
+local tremove = table.remove
+local tinsert = table.insert
 
 local Timer = new_tab(0, 1 << 10)
 
-local TIMER_LIST = new_tab(1 << 10, 0)
+local Tlist = new_tab(1 << 10, 0)
 
--- 内部函数防止被误用
+-- 创建`Timer`对象
 local function Timer_new()
-  if #TIMER_LIST > 0 then
-    return remove(TIMER_LIST)
-  end
-  return ti_new()
+  return tremove(Tlist) or ti_new()
 end
 
-local function Timer_release(t)
-  ti_stop(t)
-  TIMER_LIST[#TIMER_LIST+1] = t
+-- 停止`Timer`对象
+local function Timer_stop(self)
+  Timer[self] = nil
+  tinsert(Tlist, self.t)
+  ti_stop(self.t)
+  return true
 end
 
+-- 启动`Timer`对象
 local function Timer_start(self)
   Timer[self] = self
   ti_start(self.t, self.timeout or self.repeats, self.co)
   return self
 end
 
-local function Timer_stop(self)
-  if self and not self.stoped then
-    self.stoped = true
-    if self.t then
-      Timer_release(self.t)
-      self.t = nil
-    end
-    Timer[self] = nil
-  end
-end
-
-function Timer.count()
-  return #TIMER_LIST
-end
-
--- 超时器 --
-function Timer.timeout(timeout, cb)
-  if type(timeout) ~= 'number' or timeout <= 0 or type(cb) ~= 'function' then
+---comment 一次性定时器
+---@param timeout   number   @超时时间
+---@param callback  function @回调函数
+function Timer.timeout(timeout, callback)
+  if type(timeout) ~= 'number' or timeout <= 0 or type(callback) ~= 'function' then
     return
   end
-  local once = {stoped = false, timeout = timeout, stop = Timer_stop, t = Timer_new(), co = nil}
-  once.co = co_new(function()
-    if once.stoped then
+  local timer = { t = Timer_new(), timeout = timeout, stoped = false }
+  -- 实现`停止定时器`的方法
+  timer.stop = function(self)
+    if not self.stoped then
+      self.stoped = true
+      Timer_stop(self)
+    end
+    return true
+  end
+  -- 实现定时器回调协程
+  timer.co = co_new(function()
+    if timer.stoped then
+      timer.co = nil
       return
     end
-    co_spawn(cb)
-    Timer_stop(once)
+    co_spawn(callback)
+    return Timer_stop(timer)
   end)
-  return Timer_start(once)
+  -- 启动定时器
+  return Timer_start(timer)
 end
 
--- 循环定时器 --
-function Timer.at(repeats, cb)
-  if type(repeats) ~= 'number' or repeats <= 0 or type(cb) ~= 'function' then
+---comment 重复定时器
+---@param repeats   number   @间隔时间
+---@param callback  function @回调函数
+function Timer.at(repeats, callback)
+  if type(repeats) ~= 'number' or repeats <= 0 or type(callback) ~= 'function' then
     return
   end
-  local at = {stoped = false, repeats = repeats, stop = Timer_stop, t = Timer_new(), co = nil}
-  at.co = co_new(function( )
-    while 1 do
-      if at.stoped then
+  local timer = { t = Timer_new(), repeats = repeats, stoped = false }
+  -- 实现`停止定时器`的方法
+  timer.stop = function(self)
+    if not self.stoped then
+      self.stoped = true
+      Timer_stop(self)
+    end
+    return true
+  end
+  -- 实现定时器回调协程
+  timer.co = co_new(function()
+    while true do
+      if timer.stoped then
+        timer.co = nil
         return
       end
-      co_spawn(cb)
+      co_spawn(callback)
       co_wait_ex()
     end
   end)
-  return Timer_start(at)
+  -- 启动定时器
+  return Timer_start(timer)
 end
 
--- 休眠 --
-function Timer.sleep(timeout)
-  if type(timeout) ~= 'number' or timeout <= 0 then
+---comment 休眠当前协程
+---@param nsleep    number    @休眠时间(毫秒)
+function Timer.sleep(nsleep)
+  if type(nsleep) ~= 'number' or nsleep <= 0 then
     return
   end
-  local sleep = {stoped = false, timeout = timeout, t = Timer_new(), co = nil }
-  local cco = co_self()
-  sleep.co = co_new(function ()
-    Timer_stop(sleep)
-    return co_wakeup(cco)
+  -- 创建`Timer`对象
+  local timer = { t = Timer_new(), main_co = co_self(), timeout = nsleep }
+  -- 实现定时器回调协程
+  timer.co = co_new(function ()
+    Timer_stop(timer)
+    timer.co = nil
+    return co_wakeup(timer.main_co)
   end)
-  Timer_start(sleep)
+  Timer_start(timer)
   return co_wait()
+end
+
+---comment 计算缓存的定时器对象数量
+---@return integer
+function Timer.count()
+  return #Tlist
 end
 
 return Timer
