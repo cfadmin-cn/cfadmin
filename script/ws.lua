330ea7a12ff6cf3df526bc918b7ae0aa5767c470
869646063@qq.com <869646063@qq.com>
2021-06-03 23:34:35 +0800

    增加websocket接入示例

--- script/ws.lua
+++ script/ws.lua
@@ -1,14 +1,19 @@
 local class = require "class"
 local cf = require "cf"
-local json = require "json"
+require "utils"
 local websocket = class("websocket")
+
 
 function websocket:ctor(opt)
     self.ws = opt.ws             -- websocket对象
+    self.args = opt.args         -- GET传递的参数
+    self.headers = opt.headers   -- HTTP请求头部
     self.send_masked = false     -- 掩码(默认为false, 不建议修改或者使用)
     self.max_payload_len = 65535 -- 最大有效载荷长度(默认为65535, 不建议修改或者使用)
     self.timeout = 15            -- 默认为一直等待, 非number类型会导致异常.
     self.count = 0
+    var_dump(self.args)
+    var_dump(self.headers)
 end
 
 function websocket:on_open()
