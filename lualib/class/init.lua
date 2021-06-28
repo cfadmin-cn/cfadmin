e6a20d827e47ec1fea988cd5fd23699fca7c233a
869646063@qq.com <869646063@qq.com>
2021-06-28 21:44:57 +0800

    增加Class库的一些特性.

--- lualib/class/init.lua
+++ lualib/class/init.lua
@@ -1,30 +1,35 @@
 local type = type
+local assert = assert
+local getmetatable = getmetatable
 local setmetatable = setmetatable
-local new_tab = require "sys".new_tab
 
--- 一个精简版的类实现
-return function (cls_name)
-  local cls = {}
-  cls.__name = cls_name
-  cls.__index = cls
-  cls.__call = function (c, ...)
-    local call = c[cls_name]
-    if type(call) ~= 'function' then
-      return
-    end
-    return call(c, ...)
+-- 所有对象的基类
+local META = { __META_CLASS__ = true }
+
+META.__index = META
+
+META.new = function (M, ...)
+  assert(META == getmetatable(M), "[Lua-CLASS ERROR]: Must use `:` to create object.")
+  local ctor = M.ctor
+  local obj = setmetatable({}, M)
+  assert(ctor and type(ctor) == 'function' and ctor, "[Lua-CLASS ERROR]: Can't find `ctor` to init.")(obj, ...)
+  return obj
+end
+
+META.__call = function (M, ...)
+  local meta = getmetatable(M)
+  if meta == META then
+    return M:new(...);
   end
-  cls.new = function (c, ...)
-    if cls ~= c then
-      return print("Please use ':' to create new object :)")
-    end
-    local obj = new_tab(0, 16)
-    local ctor = cls.ctor
-    if not ctor then
-      return print("Can't find ctor to init.")
-    end
-    ctor(obj, ...)
-    return setmetatable(obj, cls)
-  end
-  return cls
+  return assert(getmetatable(M) == META and M['__name'], "[Lua-CLASS ERROR]: Invalid class arguments.")(M, ...);
+end
+
+---comment  `Class`是内部的所有对象用来实现面向对象的方法.
+---@param cname? string                  @自定义类名
+---@param meta?  table | function | nil  @自定义类行为
+---@return table                         @返回一个类
+return function (cname, meta)
+  local cls = { __name = cname }
+  cls.__index = meta or cls
+  return setmetatable(cls, META)
 end
