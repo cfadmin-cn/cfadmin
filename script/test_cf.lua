570c1375d3dd26c9bce703b75013a9f0bc252e9b
869646063@qq.com <869646063@qq.com>
2021-07-04 00:07:37 +0800

    更新示例代码

--- script/test_cf.lua
+++ script/test_cf.lua
@@ -10,7 +10,7 @@ cf.fork(function()
 	print("fork", "我现在被唤醒了")
 end)
 
-local times, timer = 0
+local times, timer = 0, nil
 timer = cf.at(1, function()
 	if times >= 3 then
 		print("循环定时器运行次数到了.")
@@ -20,7 +20,7 @@ timer = cf.at(1, function()
 	print("定时器运行次数:", times)
 end)
 
-local timer = cf.timeout(1, function()
+cf.timeout(1, function()
 	print("一次性定时器运行.")
 end)
 
