32f87a3d093067588619ca1b1fcf45f6241e0249
869646063@qq.com <869646063@qq.com>
2021-07-03 22:00:14 +0800

    增加jwt示例代码

--- script/test_http_auth.lua
+++ script/test_http_auth.lua
@@ -4,10 +4,6 @@ local httpc = require "httpc"
 local basic_key, basic_value = httpc.basic_authorization("myusername", "mypassword")
 print(basic_key, basic_value)
 
-local jwt_header_key, jwt_header_value = httpc.jwt("mysecret", [[{"key1":"value1","key2":"value2"}]])
-print(jwt_header_key, jwt_header_value)
-
-
 -- 从httpc类库中导入并且使用
 local httpc_cls = require "httpc.class"
 local hc = httpc_cls:new {}
@@ -15,5 +11,9 @@ local hc = httpc_cls:new {}
 local basic_key, basic_value = hc:basic_authorization("myusername", "mypassword")
 print(basic_key, basic_value)
 
-local jwt_header_key, jwt_header_value = hc:jwt("mysecret", [[{"key1":"value1","key2":"value2"}]])
-print(jwt_header_key, jwt_header_value)
+-- 从json.jwt导出
+local jwt = require "json.jwt"
+local raw = '{"name":"Hello world."}'
+local enc = jwt.encode(raw, "secret", "HS256")
+local dec = jwt.decode(enc, "secret", "HS256")
+print("jwt test:", raw == dec, enc)
