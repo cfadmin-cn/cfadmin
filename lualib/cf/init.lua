e8c7a911864e80fd4d0507cbabf5340ff42286cc
869646063@qq.com <869646063@qq.com>
2021-11-07 14:21:27 +0800

    更新命名避免潜在问题

--- lualib/cf/init.lua
+++ lualib/cf/init.lua
@@ -1,12 +1,12 @@
 local Co = require "internal.Co"
-local self = Co.self
-local fork = Co.spawn
-local wait = Co.wait
-local wakeup = Co.wakeup
+local co_self = Co.self
+local co_fork = Co.spawn
+local co_wait = Co.wait
+local co_wakeup = Co.wakeup
 
 local Timer = require "internal.Timer"
 local time_at = Timer.at
-local sleep = Timer.sleep
+local time_sleep = Timer.sleep
 local time_out = Timer.timeout
 
 local type = type
@@ -33,11 +33,11 @@ end
 
 ---comment 让出协程执行权
 local function yield ()
-  local co = self()
-  fork(function ()
-    wakeup(co)
+  local co = co_self()
+  co_fork(function ()
+    co_wakeup(co)
   end)
-  return wait()
+  return co_wait()
 end
 cf.yield = yield
 
@@ -46,7 +46,7 @@ cf.yield = yield
 function cf.sleep(timeout)
   timeout = tonumber(timeout)
   if timeout and timeout > 0 then
-    return sleep(timeout)
+    return time_sleep(timeout)
   end
   return yield()
 end
@@ -54,33 +54,33 @@ end
 ---comment 获取调用此方法的协程对象.
 ---@return thread
 function cf.self ()
-  return self()
+  return co_self()
 end
 
 ---comment 让出协程
 function cf.wait()
-  return wait()
+  return co_wait()
 end
 
 ---comment  创建一个由cf框架调度的协程
 ---@param func function @协程的执行的函数
 ---@return thread 协程对象
 function cf.fork(func, ...)
-  return fork(func, ...)
+  return co_fork(func, ...)
 end
 
 ---comment 唤醒一个由cf框架创建的协程
 ---@param co thread @被唤醒的协程对象与需要传递给协程的参数
 ---@return nil
 function cf.wakeup(co, ...)
-  return wakeup(co, ...)
+  return co_wakeup(co, ...)
 end
 
 local function cb(f, ctx)
 	local ok, errinfo = pcall(f)
 	ctx.waits = ctx.waits - 1
 	if ctx.waits == 0 then
-		wakeup(ctx.co)
+		co_wakeup(ctx.co)
 	end
   if not ok then
     return error(errinfo)
@@ -92,16 +92,16 @@ end
 ---@return nil @始终不存在返回值.
 function cf.join(fn, ...)
 	local qlist = {fn, ...}
-  local ctx = { co = self(), waits = nil }
+  local ctx = { co = co_self(), waits = nil }
   local waits = 0
 	for _, f in ipairs(qlist) do
     if type(f) == 'function' then
-      fork(cb, f, ctx)
+      co_fork(cb, f, ctx)
       waits = waits + 1
     end
 	end
   ctx.waits = waits
-	return wait()
+	return co_wait()
 end
 
 return cf
