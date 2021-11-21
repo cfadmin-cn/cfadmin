8348bcf79e3b8fc2c28f6e8443f5cdf9ff7f5992
869646063@qq.com <869646063@qq.com>
2021-11-21 22:48:19 +0800

    去除mqtt的警告

--- lualib/protocol/mqtt/bit.lua
+++ lualib/protocol/mqtt/bit.lua
@@ -1,9 +1,4 @@
 -- wrapper around BitOp module
-
-if _VERSION == "Lua 5.1" or _VERSION == "Lua 5.2" then
-	return require("bit")
-else
-	return require("protocol.mqtt.bit53")
-end
+return require("protocol.mqtt.bit53")
 
 -- vim: ts=4 sts=4 sw=4 noet ft=lua
