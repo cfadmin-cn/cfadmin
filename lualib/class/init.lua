b297ef1011989c40b6002a4ae5014292746a4955
CandyMi <869646063@qq.com>
2025-02-27 16:55:52 +0800

    同步class与var_dump内容

--- lualib/class/init.lua
+++ lualib/class/init.lua
@@ -1,22 +1,107 @@
-local type, pairs = type, pairs
+local pairs, type = pairs, type
 local getmetatable = getmetatable
 local setmetatable = setmetatable
 
-local metaclass = require "class.meta"
+local V = tonumber(_VERSION:sub(4))
 
----comment  `class`是内部的所有对象用来实现面向对象的方法.
----@param cname? string                  @自定义类名
----@param cmeta?  table  | nil            @自定义父类
----@return table                         @返回一个类
-return function (cname, cmeta)
-  local cls = { __name = cname }
-  if getmetatable(cmeta) == metaclass then
+-- 这里定义的字段不会被主动继承
+local inheritance = { __name = true, __index = true, __metatable = true }
+
+---@class META
+local Anonymous = { __name = "Anonymous", ctor = nil }
+
+---comment 索引自身
+Anonymous.__index = Anonymous
+
+---comment 字符串化
+function Anonymous.__tostring(M, ...)
+  if V and V > 5.3 then
+    return string.format("%s: %p", M.__name, M)
+  end
+  return M.__name
+end
+
+local function class_init(M, ...)
+  if getmetatable(M) ~= Anonymous then
+    error("[class error]: You must use the `cls(...)` to create an object.", 2)
+  end
+  local obj = setmetatable({}, M)
+  local ctor = obj.ctor
+  if type(ctor) == 'function' then
+    ctor(obj, ...)
+  end
+  return obj
+end
+
+---comment 使用cls(...)语法创建对象
+---@generic T : META
+---@param M   META
+---@param ... any
+---@return T
+function Anonymous.__call(M, ...)
+  return class_init(M, ...)
+end
+
+---comment 使用cls:new(...)语法创建对象
+---@generic T : META
+---@param M   META
+---@param ... any
+---@return T
+---@deprecated
+function Anonymous.new(M, ...)
+  return class_init(M, ...)
+end
+
+---comment  此函数返回`Lua`类, 所有内部`class`实现均使用它.
+---@param cname string?    @自定义类名(可选)
+---@param cmeta META?      @继承的父类(可选)
+---@return META
+local function class_define(cname, cmeta)
+  local cls = {}
+  if cmeta and getmetatable(cmeta) == Anonymous then
     for k, v in pairs(cmeta) do
-      if type(v) == 'function' or (k ~= '__name' and k ~= '__index' and k ~= '__metatable') then
+      if not inheritance[k] then
         cls[k] = v
       end
     end
   end
-  cls.__index = cls
-  return setmetatable(cls, metaclass)
+  cls.__index = cls; cls.__name = cname;
+  return setmetatable(cls, Anonymous)
 end
+
+---@class ClassWraper
+local class = { __record = false }
+class.__index = class
+
+function class:__call(cname, cmeta)
+  -- 记录类信息
+  if self.__record then
+    local list = self.__list__
+    if not self.__list__ then
+      list = {}
+      self.__list__ = list
+    end
+    local info = debug.getinfo(2)
+    if cname and cmeta then
+      list[#list+1] = string.format("class `%s`: public `%s` (%s:%s)", cname, tostring(cmeta):gsub(':(.*)', ''), info.short_src, info.currentline)
+    else
+      list[#list+1] = string.format("class `%s` (%s:%s)", cname and tostring(cname) or 'Unknown', info.short_src, info.currentline)
+    end
+  end
+  -- 记录类信息
+  return class_define(cname, cmeta)
+end
+
+---comment 返回对象引用信息数组
+---@return table
+function class:dump()
+  return self.__list__
+end
+
+---comment 当前服务对象注册记录
+---@param op boolean @默认为`false`
+function class:enable_recorded(op)
+  self.__record = op
+end
+
+return setmetatable({}, class)
--- lualib/utils/init.lua
+++ lualib/utils/init.lua
@@ -1,47 +1,133 @@
-require "utils.string"
-require "utils.table"
+local type = type
+local pairs = pairs
+local print = print
+local tostring = tostring
 
--- 格式化输出(美化)
-_G["var_dump"] = function (data, showMetatable, lastCount)
-    if type(data) ~= "table" then
-        --Value
-        if type(data) == "string" then
-            io.write('"', data, '"')
+local getmetatable = getmetatable
+local setmetatable = setmetatable
+
+local mtype = math.type or function (v)
+  return type(v) == 'number' and
+    (v % 1 == 0 and 'integer' or 'float') or 'nil'
+end
+
+local debug_getinfo = debug.getinfo
+
+local strrep = string.rep
+local strfmt = string.format
+
+local tconcat = table.concat
+local tinsert = table.insert
+
+local r = debug.getregistry()
+local l, g = r._LOADED, _G
+
+local V = tonumber(_VERSION:sub(4))
+
+local function space(level)
+  return strrep('    ', level)
+end
+
+local function tokey(key)
+  if type(key) ~= 'string' then
+    return strfmt("%s", key)
+  end
+  return strfmt("%q", key)
+end
+
+local array_mt = r['Lua.Array']
+local function isarray(tab)
+  if #tab > 0 then
+    return true
+  end
+  return array_mt and getmetatable(tab) == array_mt
+end
+
+---comment 格式化打印变量`tab`.
+---@param tab   any        @指定指定变量
+---@param meta  boolean?   @是否跟进元表
+---@param level integer?   @指定打印层级
+local function var_dump(tab, meta, level)
+  if type(tab) ~= 'table' then
+    return strfmt("%s\n", tostring(tab))
+  end
+  local ptab = {}
+  -- 拆分
+  local M, I
+  if meta then
+    M = getmetatable(tab)
+    if M then
+      setmetatable(tab, nil)
+      tab.__metatable__ = M
+    end
+  end
+  if tab.__index == tab then
+    I = tab.__index
+    tab.__index = nil
+  end
+  local n = 0
+  local olevel = level
+  level = level + 1
+  for k, v in pairs(tab) do
+    if type(k) ~= 'number' and type(k) ~= 'string' then
+      k = tostring(k)
+    end
+    if type(v) == "number" then
+      if mtype(v) == 'float' then
+        tinsert(ptab, strfmt('%s[%s] = Number(%s),\n', space(level), tokey(k), v))
+      else
+        tinsert(ptab, strfmt('%s[%s] = Integer(%d),\n', space(level), tokey(k), v))
+      end
+    elseif type(v) == 'boolean' then
+      tinsert(ptab, strfmt('%s[%s] = Boolean(%s),\n', space(level), tokey(k), v))
+    elseif type(v) == 'table' then
+      if v == g or v == l then
+        tinsert(ptab, strfmt('%s[%s] = %s,\n', space(level), tokey(k), tostring(v)))
+      else
+        tinsert(ptab, strfmt('%s[%s] = %s', space(level), tokey(k), var_dump(v, meta, level)))
+      end
+    elseif type(v) == 'string' then
+      tinsert(ptab, strfmt('%s[%s] = String(%q),\n', space(level), tokey(k), v))
+    elseif type(v) == 'function' then
+      local info = debug_getinfo(v)
+      if info.linedefined > 0 then
+        if V > 5.3 then
+          tinsert(ptab, strfmt('%s[%s] = LuaFunction(%p%s),\n', space(level), tokey(k), v, info.source .. ':' .. info.linedefined))
         else
-            io.write(tostring(data))
+          tinsert(ptab, strfmt('%s[%s] = %s(%s),\n', space(level), tokey(k), v, info.source .. ':' .. info.linedefined))
         end
+      else
+        if V > 5.3 then
+          tinsert(ptab, strfmt('%s[%s] = LuaCFunction(%p),\n', space(level), tokey(k), tostring(v)))
+        else
+          tinsert(ptab, strfmt('%s[%s] = c%s,\n', space(level), tokey(k), tostring(v)))
+        end
+      end
     else
-        --Format
-        local count = lastCount or 0
-        count = count + 1
-        io.write("{\n")
-        --Metatable
-        if showMetatable then
-            for i = 1,count do io.write("      ") end
-            local mt = getmetatable(data)
-            io.write("\"__metatable\" = ")
-            var_dump(mt, showMetatable, count)    -- 如果不想看到元表的元表，可将showMetatable处填nil
-            io.write(",\n")     				     --如果不想在元表后加逗号，可以删除这里的逗号
-        end
-        --Key
-        for key,value in pairs(data) do
-            for i = 1,count do io.write("      ") end
-            if type(key) == "string" then
-                io.write('["', key, '"] = ')
-            elseif type(key) == "number" then
-                io.write("[", key, "] = ")
-            else
-                io.write(tostring(key))
-            end
-            var_dump(value, showMetatable, count) -- 如果不想看到子table的元表，可将showMetatable处填nil
-            io.write(",\n")     					 --如果不想在table的每一个item后加逗号，可以删除这里的逗号
-        end
-        --Format
-        for i = 1, lastCount or 0 do io.write("      ") end
-        io.write("}")
+      tinsert(ptab, strfmt('%s[%s] = %s,\n', space(level), tokey(k), tostring(v)))
     end
-    --Format
-    if not lastCount then
-        io.write("\n")
+    n = n + 1
+  end
+  -- 还原
+  if meta then
+    if M then
+      setmetatable(tab, M)
+      tab.__metatable__ = nil
     end
+  end
+  if I then
+    tab.__index = I
+  end
+  local left, right = "{\n", "%s}%s\n"
+  if n == #tab and isarray(tab) then
+    left, right = "[\n", "%s]%s\n"
+  end
+  return left .. tconcat(ptab) .. strfmt(right, space(olevel), olevel == 0 and "" or ",")
+end
+
+---comment Dump表结构
+---@param tab    any       @格式化打印当前表结构
+---@param meta?  boolean   @将元表结构也打印出来
+_G.var_dump = function (tab, meta)
+  print(var_dump(tab, meta, 0))
 end
